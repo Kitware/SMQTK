f86d1303d7a03e210a38a50d300e68795d681a69
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2015-02-26 17:57:17 -0500

    Added map

--- src/main.js
+++ src/main.js
@@ -16,4 +16,22 @@ $(function () {
       baseUrl: 'http://otile1.mqcdn.com/tiles/1.0.0/map/'
     }
   );
+
+  $.ajax( "/api/v1/data?limit=50000" )
+  .done(function(data) {
+    map
+      .createLayer('feature')
+        .createFeature('point')
+          .data(data)
+          .position(function(d) {return {x:d.field7, y:d.field6}})
+            .style('radius', 10)
+            .style('fillColor', "red")
+            .style('fillOpacity', 0.01)
+            .style('strokeColor', {r: 0.3, g: 0.3, b: 0.3})
+            .style('stroke', 0);
+    map.draw();
+  })
+  .fail(function() {
+    alert( "error" );
+  });
 });

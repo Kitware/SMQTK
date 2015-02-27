02aef11a26c0b6ca806fb0226268bd1209367184
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2015-02-27 11:37:58 -0500

    Adding capability to aggregate data

--- src/main.js
+++ src/main.js
@@ -1,6 +1,26 @@
 $(function () {
   'use strict';
 
+  function updateDataLayer() {
+  }
+
+  function aggregateByLocation() {
+    var dataGroupedByLocation, key, locationBin = {}, aggData;
+    if (mdata) {
+      aggData = mdata.map(function(item) {
+        key = item.field7 + '|' + item.field6;
+        if (item.binCount === undefined) {
+          item.binCount = 1;
+          locationBin[key] = 1;
+          return item;
+        } else if (key in locationBin) {
+          locationBin[key] += 1;
+          item.binCount = locationBin[key];
+        }
+      });
+    }
+  }
+
   var map = geo.map({
     node: '#map',
     center: {
@@ -8,7 +28,7 @@ $(function () {
       y: 39.5
     },
     zoom: 1
-  });
+  }), locationBin = null, mdata = null, aggData = null;
 
   map.createLayer(
     'osm',
@@ -19,10 +39,13 @@ $(function () {
 
   $.ajax( "/api/v1/data?limit=10000" )
   .done(function(data) {
+    // Cache the data for later
+    mdata = data;
+    aggregateByLocation();
     map
       .createLayer('feature')
         .createFeature('point')
-          .data(data)
+          .data(aggData)
           .position(function(d) {return {x:d.field7, y:d.field6}})
           .style('stroke', false)
           .style('fillOpacity', 0.05)

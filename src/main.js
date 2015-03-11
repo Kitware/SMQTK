63ed71cd757decb3b76894519e9f67ad5dba3649
Aashish Chaudhary <aashish.chaudhary@kitware.com>
2015-03-11 12:50:04 -0400

    Added animation infrastructure

--- src/main.js
+++ src/main.js
@@ -1,6 +1,23 @@
 $(function () {
   'use strict';
 
+  // Globals
+  var map = geo.map({
+    node: '#map',
+    center: {
+      x: -98.0,
+      y: 39.5
+    },
+    zoom: 1
+  }), locationBin = null, scale = null, pointFeature, ready = false, startTime = null;
+
+  map.createLayer(
+    'osm',
+    {
+      baseUrl: 'http://c.tile.stamen.com/terrain-labels/'
+    }
+  );
+
   // Aggregate data
   function aggregateByLocation(data) {
     var dataGroupedByLocation, key, locationBin = {}, min = 0, max = 1, newdata = [];
@@ -25,9 +42,9 @@ $(function () {
 
   // Query given a time duration
   function queryData(timerange, callback) {
-    console.log("/api/v1/data?limit=1000000&duration=["+timerange+"]");
+    console.log("/api/v1/data?limit=100000&duration=["+timerange+"]");
 
-    $.ajax("/api/v1/data?limit=1000000&duration=["+timerange+"]")
+    $.ajax("/api/v1/data?limit=100000&duration=["+timerange+"]")
       .done(function(data) {
         console.log(data.length);
         if (callback !== undefined) {
@@ -40,7 +57,7 @@ $(function () {
   }
 
   // Create geovis
-  function createVis(data) {
+  function createVis(data, callback) {
     var aggdata = aggregateByLocation(data);
     console.log(aggdata);
     scale = d3.scale.linear().domain([aggdata.min, aggdata.max])
@@ -59,24 +76,53 @@ $(function () {
       .style('fillColor', "orange");
 
     map.draw();
+
+    if (callback) {
+      callback();
+    }
   }
 
-  // Globals
-  var map = geo.map({
-    node: '#map',
-    center: {
-      x: -98.0,
-      y: 39.5
-    },
-    zoom: 1
-  }), locationBin = null, scale = null, pointFeature;
+  // Create animation
+  function runAnimation(timestamp) {
+    if (ready) {
+      // First get the values from the slider
+      var range = $( "#slider" ).slider( "values" ),
+          min = $( "#slider" ).slider( "option", "min" ),
+          max = $( "#slider" ).slider( "option", "max" ),
+          delta = range[1] - range[0],
+          stopAnimation = false,
+          newRange = [ range[ 0 ] + delta, range[ 1 ] + delta ];
 
-  map.createLayer(
-    'osm',
-    {
-      baseUrl: 'http://c.tile.stamen.com/terrain-labels/'
+      if (newRange[0] >= max) {
+        newRange[0] = max;
+        stopAnimation = true;
+      }
+      if (newRange[0] <= min) {
+        newRange[0] = min;
+        stopAnimation = true;
+      }
+      if (newRange[1] >= max) {
+        newRange[1] = max;
+        stopAnimation = true;
+      }
+      if (newRange[1] <= min) {
+        newRange[1] = min;
+        stopAnimation = true;
+      }
+
+      // Set the slider value
+      $( "#slider" ).slider( "option", "values", newRange );
+
+      // Query the data and create vis again
+      queryData( newRange, function(data) {
+        createVis(data, function() {
+          if (!stopAnimation) {
+            window.requestAnimationFrame(runAnimation);
+          }
+        });
+      });
     }
-  );
+  }
 
   $.ajax( "/api/v1/data" )
     .done(function(range) {
@@ -120,6 +166,11 @@ $(function () {
 
     // Now query data
     queryData($("#slider").slider("values"), createVis);
+
+    ready = true;
+
+    // Testing
+    window.requestAnimationFrame(runAnimation);
   })
   .fail(function() {
     console.log('failed');

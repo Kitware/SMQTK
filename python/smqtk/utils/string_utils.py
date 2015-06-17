1ac5b3aafa46cac740e52989c2fd970c3a53ed0e
Paul Tunison <paul.tunison@kitware.com>
2015-06-17 13:00:50 -0400

    Updated implementation to act more predictably
    Now always returns the number of segments requested, except for when
    number of segments exceeds string length.

--- python/smqtk/utils/string_utils.py
+++ python/smqtk/utils/string_utils.py
@@ -4,16 +4,27 @@ __author__ = 'purg'
 def partition_string(s, segments):
     """
     Partition a string into a number of segments. If the given number of
-    segments does not divide evenly into the string's length, this function
-    behaves erratically.
+    segments does not divide evenly into the string's length, extra characters
+    are added to the leading segments in order to allow for the requested number
+    of segments.
 
     This is useful when partitioning an MD5 sum of a file to determine where it
     should be placed in a directory tree system.
 
+    >>> partition_string("foobar", 2)
+    ['foo', 'bar']
+    >>> partition_string("foobar", 4)
+    ['fo', 'ob', 'a', 'r']
+    >>> partition_string('d7ca25c5-b886-4a1b-87fe-5945313d350b', 11)
+    ['d7ca', '25c5', '-b88', '6-4', 'a1b', '-87', 'fe-', '594', '531', '3d3', '50b']
+
+    :raises AssertionError: Segmentation value greater than the length of the
+        string.
+
     :param s: String to partition.
     :type s: str
 
-    :param segments: Number of segments to splut the string into
+    :param segments: Number of segments to split the string into
     :type segments: int
 
     :return: A list of N segments. If the given number of segments does not
@@ -22,11 +33,18 @@ def partition_string(s, segments):
     :rtype: list[str]
 
     """
+    assert segments <= len(s), \
+        "Cannot split given string into more segments than there are " \
+        "characters in the string!"
+
     seg_len = len(s) // segments
-    tail = len(s) % segments
+    extra_iters = len(s) % segments
+
     r = []
-    for i in range(segments):
-        r.append(s[i*seg_len:(i+1)*seg_len])
-    if tail:
-        r.append(s[-tail:])
+    i = 0
+    for k in range(segments):
+        j = i + seg_len + (k < extra_iters)
+        r.append(s[i:j])
+        i = j
+
     return r

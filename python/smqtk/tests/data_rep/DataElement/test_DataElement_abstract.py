7dfa3c8167f3cb575d1dc688efb523efcf40bdf3
Paul Tunison <paul.tunison@kitware.com>
2015-06-19 17:55:06 -0400

    Upgraded DataElement tmep file writing/management + tests

--- python/smqtk/tests/data_rep/DataElement/test_DataElement_abstract.py
+++ python/smqtk/tests/data_rep/DataElement/test_DataElement_abstract.py
@@ -4,142 +4,213 @@ functionality.
 """
 __author__ = 'purg'
 
+import hashlib
 import mock
 import nose.tools as ntools
+import os.path as osp
+import tempfile
 import unittest
 
 import smqtk.data_rep.data_element_abstract
+
+
+# because this has a stable mimetype conversion
+EXPECTED_CONTENT_TYPE = "image/png"
+EXPECTED_BYTES = "hello world"
+EXPECTED_UUID = 1234567890
+EXPECTED_MD5 = hashlib.md5(EXPECTED_BYTES).hexdigest()
 
 
 class DummyDataElement (smqtk.data_rep.data_element_abstract.DataElement):
     # abstract methods have no base functionality
 
     def content_type(self):
-        return 'image/png'
+        return EXPECTED_CONTENT_TYPE
 
     def get_bytes(self):
         # Aligned with the MD5 string in test class setUp method
-        return "hello world"
+        return EXPECTED_BYTES
 
     def uuid(self):
-        return 1234567890
+        return EXPECTED_UUID
 
 
 class TestDataElementAbstract (unittest.TestCase):
 
-    def setUp(self):
-        self.de = DummyDataElement()
-        self.expected_uuid = 1234567890
-        self.expected_md5 = '5eb63bbbe01eeed093cb22bb8f5acdc3'
-
     def test_md5(self):
-        ntools.assert_is_none(self.de._md5_cache)
+        de = DummyDataElement()
 
-        md5 = self.de.md5()
+        ntools.assert_is_none(de._md5_cache)
 
-        ntools.assert_is_not_none(self.de._md5_cache)
-        ntools.assert_equal(self.de._md5_cache, self.expected_md5)
-        ntools.assert_equal(md5, self.expected_md5)
+        md5 = de.md5()
+
+        ntools.assert_is_not_none(de._md5_cache)
+        ntools.assert_equal(de._md5_cache, EXPECTED_MD5)
+        ntools.assert_equal(md5, EXPECTED_MD5)
 
         # When called a second time, should use cache instead of recomputing
         with mock.patch("smqtk.data_rep.data_element_abstract.hashlib") as mock_hashlib:
-            md5 = self.de.md5()
+            md5 = de.md5()
             ntools.assert_false(mock_hashlib.md5.called)
-            ntools.assert_equal(md5, self.expected_md5)
-
-    @mock.patch('__builtin__.file')
-    @mock.patch('smqtk.data_rep.data_element_abstract.safe_create_dir')
-    @mock.patch('__builtin__.open')
-    @mock.patch('smqtk.data_rep.data_element_abstract.tempfile')
-    def test_writeTemp(self, mock_tempfile, mock_open, mock_scd,
-                       mock_file):
-        expected_tempfile_path = '/path/to/tmpfile'
-
-        mock_tempfile.mkstemp.return_value = (0, expected_tempfile_path)
-        mock_open.return_value = mock_file()
-
-        with mock.patch('smqtk.data_rep.data_element_abstract.os'):
-            path = self.de.write_temp()
-
-        ntools.assert_false(mock_scd.called)
-        ntools.assert_true(mock_tempfile.mkstemp.called)
-        mock_tempfile.mkstemp.assert_called_once_with(suffix='.png', dir=None)
-        mock_open.assert_called_once_with(expected_tempfile_path, 'wb')
-        mock_file().__enter__().write.assert_called_once_with(self.de.get_bytes())
-        ntools.assert_equal(path, expected_tempfile_path)
-
-        # Running it again should result in the same return, but system calls
-        # should NOT be made (caching)
-        mock_tempfile.reset_mock()
-        mock_open.reset_mock()
-        mock_scd.reset_mock()
-        mock_file.reset_mock()
-
-        with mock.patch('smqtk.data_rep.data_element_abstract.os'):
-            path = self.de.write_temp()
-
-        ntools.assert_false(mock_scd.called)
-        ntools.assert_false(mock_tempfile.mkstemp.called)
-        ntools.assert_false(mock_open.called)
-        ntools.assert_false(mock_file().__enter__().write.called)
-        ntools.assert_equal(path, expected_tempfile_path)
-
-    @mock.patch('smqtk.data_rep.data_element_abstract.safe_create_dir')
-    @mock.patch('smqtk.data_rep.data_element_abstract.os')
-    @mock.patch('smqtk.data_rep.data_element_abstract.tempfile')
-    @mock.patch('__builtin__.file')
-    @mock.patch('__builtin__.open')
-    def test_writeTemp_specificDir(self, mock_open, mock_file, mock_tempfile,
-                                   mosck_os, mock_scd):
-        # Same as above test, but telling it to place in specific directory
-        mock_tempfile.mkstemp.return_value = 0, 'some/dir/t'
-
-        path = 'some/dir'
-        self.de.write_temp(path)
-
-        ntools.assert_true(mock_scd.called)
-        mock_scd.assert_called_once_with(path)
-
-    @mock.patch("smqtk.data_rep.data_element_abstract.os")
-    def test_cleanTemp_noTemp(self, mock_os):
-        # should do all of nothing
-        ntools.assert_false(hasattr(self.de, '_temp_filepath'))
-
-        self.de.clean_temp()
-
-        ntools.assert_false(mock_os.path.isfile.called)
-        ntools.assert_false(mock_os.remove.called)
-
-    @mock.patch("smqtk.data_rep.data_element_abstract.os")
-    def test_cleanTemp_hasTemp_noPath(self, mock_os):
-        self.de._temp_filepath = None
-        mock_os.path.isfile.return_value = False
-
-        self.de.clean_temp()
-
-        ntools.assert_true(mock_os.path.isfile.called)
-        mock_os.path.isfile.assert_called_once_with(None)
-        ntools.assert_false(mock_os.remove.called)
-
-    @mock.patch("smqtk.data_rep.data_element_abstract.os")
-    def test_cleanTemp_hasTemp_noPath(self, mock_os):
-        expected_path = '/tmp/something'
-        self.de._temp_filepath = expected_path
-        mock_os.path.isfile.return_value = True
-
-        self.de.clean_temp()
-
-        ntools.assert_true(mock_os.path.isfile.called)
-        mock_os.path.isfile.assert_called_once_with(expected_path)
-        ntools.assert_true(mock_os.remove.called)
-        mock_os.remove.assert_called_once_with(expected_path)
+            ntools.assert_equal(md5, EXPECTED_MD5)
 
     def test_del(self):
-        m_clean_temp = self.de.clean_temp = mock.Mock()
-        del self.de
+        de = DummyDataElement()
+        m_clean_temp = de.clean_temp = mock.Mock()
+        del de
 
         ntools.assert_true(m_clean_temp.called)
 
     def test_hashing(self):
         # Hash should be that of the UUID of the element
-        ntools.assert_equal(hash(self.de), hash(self.expected_uuid))
+        de = DummyDataElement()
+        ntools.assert_equal(hash(de), hash(EXPECTED_UUID))
+
+    # Cases:
+    #   - no existing temps, no specific dir
+    #   - no existing temps, given specific dir
+    #   - existing temps, no specific dir
+    #   - existing temps, given specific dir
+    #
+    # Mocking open, os.open, os.close and fcntl to actual file interaction
+    #   - fcntl is used under the hood of tempfile to open a file (which also
+    #       creates it on disk).
+
+    @mock.patch('smqtk.data_rep.data_element_abstract.safe_create_dir')
+    @mock.patch('fcntl.fcntl')  # global
+    @mock.patch('os.close')  # global
+    @mock.patch('os.open')  # global
+    @mock.patch('__builtin__.open')
+    def test_writeTemp_noExisting_noDir(self,
+                                        mock_open, mock_os_open, mock_os_close,
+                                        mock_fcntl, mock_scd):
+        # no existing temps, no specific dir
+        fp = DummyDataElement().write_temp()
+
+        ntools.assert_false(mock_scd.called)
+        ntools.assert_true(mock_open.called)
+        ntools.assert_equal(osp.dirname(fp), tempfile.gettempdir())
+
+    @mock.patch('smqtk.data_rep.data_element_abstract.safe_create_dir')
+    @mock.patch('fcntl.fcntl')  # global
+    @mock.patch('os.close')  # global
+    @mock.patch('os.open')  # global
+    @mock.patch('__builtin__.open')
+    def test_writeTemp_noExisting_givenDir(self,
+                                           mock_open, mock_os_open,
+                                           mock_os_close, mock_fcntl, mock_scd):
+        # no existing temps, given specific dir
+        target_dir = '/some/dir/somewhere'
+
+        fp = DummyDataElement().write_temp(target_dir)
+
+        mock_scd.assert_called_once_with(target_dir)
+        ntools.assert_true(mock_open.called)
+        ntools.assert_not_equal(osp.dirname(fp), tempfile.gettempdir())
+        ntools.assert_equal(osp.dirname(fp), target_dir)
+
+    @mock.patch('smqtk.data_rep.data_element_abstract.safe_create_dir')
+    @mock.patch('fcntl.fcntl')  # global
+    @mock.patch('os.close')  # global
+    @mock.patch('os.open')  # global
+    @mock.patch('__builtin__.open')
+    def test_writeTemp_hasExisting_noDir(self,
+                                         mock_open, mock_os_open, mock_os_close,
+                                         mock_fcntl, mock_scd):
+        # existing temps, no specific dir
+        prev_0 = '/tmp/file.txt'
+        prev_1 = '/tmp/file_two.png'
+
+        de = DummyDataElement()
+        de._temp_filepath_stack.append(prev_0)
+        de._temp_filepath_stack.append(prev_1)
+
+        fp = de.write_temp()
+
+        ntools.assert_false(mock_scd.called)
+        ntools.assert_false(mock_open.called)
+        ntools.assert_equal(fp, prev_1)
+
+    @mock.patch('smqtk.data_rep.data_element_abstract.safe_create_dir')
+    @mock.patch('fcntl.fcntl')  # global
+    @mock.patch('os.close')  # global
+    @mock.patch('os.open')  # global
+    @mock.patch('__builtin__.open')
+    def test_writeTemp_hasExisting_givenNewDir(self, mock_open, mock_os_open,
+                                               mock_os_close, mock_fcntl,
+                                               mock_scd):
+        # existing temps, given specific dir
+        prev_0 = '/tmp/file.txt'
+        prev_1 = '/tmp/file_two.png'
+
+        target_dir = '/some/specific/dir'
+
+        de = DummyDataElement()
+        de._temp_filepath_stack.append(prev_0)
+        de._temp_filepath_stack.append(prev_1)
+
+        fp = de.write_temp(temp_dir=target_dir)
+
+        ntools.assert_true(mock_scd.called)
+        ntools.assert_true(mock_open.called)
+        ntools.assert_equal(osp.dirname(fp), target_dir)
+
+    @mock.patch('smqtk.data_rep.data_element_abstract.safe_create_dir')
+    @mock.patch('fcntl.fcntl')  # global
+    @mock.patch('os.close')  # global
+    @mock.patch('os.open')  # global
+    @mock.patch('__builtin__.open')
+    def test_writeTemp_hasExisting_givenExistingDir(self, mock_open,
+                                                    mock_os_open, mock_os_close,
+                                                    mock_fcntl, mock_scd):
+        # existing temps, given specific dir already in stack
+        prev_0 = '/dir1/file.txt'
+        prev_1 = '/tmp/things/file_two.png'
+        prev_2 = '/some/specific/dir'
+
+        de = DummyDataElement()
+        de._temp_filepath_stack.append(prev_0)
+        de._temp_filepath_stack.append(prev_1)
+        de._temp_filepath_stack.append(prev_2)
+
+        target_dir = "/tmp/things"
+
+        fp = de.write_temp(temp_dir=target_dir)
+
+        ntools.assert_false(mock_scd.called)
+        ntools.assert_false(mock_open.called)
+        ntools.assert_equal(fp, prev_1)
+
+    @mock.patch("smqtk.data_rep.data_element_abstract.os")
+    def test_cleanTemp_noTemp(self, mock_os):
+        # should do all of nothing
+        de = DummyDataElement()
+
+        de.clean_temp()
+
+        ntools.assert_false(mock_os.path.isfile.called)
+        ntools.assert_false(mock_os.remove.called)
+
+    @mock.patch("smqtk.data_rep.data_element_abstract.os")
+    def test_cleanTemp_hasTemp_badPath(self, mock_os):
+        de = DummyDataElement()
+        de._temp_filepath_stack.append('tmp/thing')
+        mock_os.path.isfile.return_value = False
+
+        de.clean_temp()
+
+        mock_os.path.isfile.assert_called_once_with('tmp/thing')
+        ntools.assert_false(mock_os.remove.called)
+
+    @mock.patch("smqtk.data_rep.data_element_abstract.os")
+    def test_cleanTemp_hasTemp_validPath(self, mock_os):
+        expected_path = '/tmp/something'
+
+        de = DummyDataElement()
+        de._temp_filepath_stack.append(expected_path)
+        mock_os.path.isfile.return_value = True
+
+        de.clean_temp()
+
+        mock_os.path.isfile.assert_called_once_with(expected_path)
+        mock_os.remove.assert_called_once_with(expected_path)

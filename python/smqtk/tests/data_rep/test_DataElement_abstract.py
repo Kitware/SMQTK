fb965a6f31bb3ebd3e921b34829c5df8b5638e19
Paul Tunison <paul.tunison@kitware.com>
2015-06-19 11:57:55 -0400

    Added a couple more tests for data elem abstract

--- python/smqtk/tests/data_rep/test_DataElement_abstract.py
+++ python/smqtk/tests/data_rep/test_DataElement_abstract.py
@@ -15,20 +15,21 @@ class DummyDataElement (smqtk.data_rep.data_element_abstract.DataElement):
     # abstract methods have no base functionality
 
     def content_type(self):
-        return 'text/plain'
+        return 'image/png'
 
     def get_bytes(self):
+        # Aligned with the MD5 string in test class setUp method
         return "hello world"
 
     def uuid(self):
-        return self.md5()
+        return 1234567890
 
 
 class TestDataElementAbstract (unittest.TestCase):
 
     def setUp(self):
         self.de = DummyDataElement()
-
+        self.expected_uuid = 1234567890
         self.expected_md5 = '5eb63bbbe01eeed093cb22bb8f5acdc3'
 
     def test_md5(self):
@@ -62,12 +63,13 @@ class TestDataElementAbstract (unittest.TestCase):
 
         ntools.assert_false(mock_scd.called)
         ntools.assert_true(mock_tempfile.mkstemp.called)
+        mock_tempfile.mkstemp.assert_called_once_with(suffix='.png', dir=None)
         mock_open.assert_called_once_with(expected_tempfile_path, 'wb')
         mock_file().__enter__().write.assert_called_once_with(self.de.get_bytes())
         ntools.assert_equal(path, expected_tempfile_path)
 
         # Running it again should result in the same return, but system calls
-        # should be made (caching)
+        # should NOT be made (caching)
         mock_tempfile.reset_mock()
         mock_open.reset_mock()
         mock_scd.reset_mock()
@@ -81,6 +83,22 @@ class TestDataElementAbstract (unittest.TestCase):
         ntools.assert_false(mock_open.called)
         ntools.assert_false(mock_file().__enter__().write.called)
         ntools.assert_equal(path, expected_tempfile_path)
+
+    @mock.patch('smqtk.data_rep.data_element_abstract.safe_create_dir')
+    @mock.patch('smqtk.data_rep.data_element_abstract.os')
+    @mock.patch('smqtk.data_rep.data_element_abstract.tempfile')
+    @mock.patch('__builtin__.file')
+    @mock.patch('__builtin__.open')
+    def test_writeTemp_specificDir(self, mock_open, mock_file, mock_tempfile,
+                                   mosck_os, mock_scd):
+        # Same as above test, but telling it to place in specific directory
+        mock_tempfile.mkstemp.return_value = 0, 'some/dir/t'
+
+        path = 'some/dir'
+        self.de.write_temp(path)
+
+        ntools.assert_true(mock_scd.called)
+        mock_scd.assert_called_once_with(path)
 
     @mock.patch("smqtk.data_rep.data_element_abstract.os")
     def test_cleanTemp_noTemp(self, mock_os):
@@ -115,3 +133,13 @@ class TestDataElementAbstract (unittest.TestCase):
         mock_os.path.isfile.assert_called_once_with(expected_path)
         ntools.assert_true(mock_os.remove.called)
         mock_os.remove.assert_called_once_with(expected_path)
+
+    def test_del(self):
+        m_clean_temp = self.de.clean_temp = mock.Mock()
+        del self.de
+
+        ntools.assert_true(m_clean_temp.called)
+
+    def test_hashing(self):
+        # Hash should be that of the UUID of the element
+        ntools.assert_equal(hash(self.de), hash(self.expected_uuid))

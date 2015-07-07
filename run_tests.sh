127d505cb593e00763e01e9212bb249db56c8a08
Jonathan Owens <jonathan.owens@kitware.com>
2015-07-07 14:42:31 -0400

    tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,2 @@
-#!/usr/bin/env sh
-nosetests --with-doctest --with-coverage --cover-package=smqtk --exclude-dir-file=nose_exclude_dirs.txt python/smqtk
+#! /usr/bin/env sh
+nosetests --with-doctest --with-coverage --cover-package=smqtk --nocapture python/smqtk --exclude-dir-file=nose_exclude_dirs.txt

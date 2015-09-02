86399bdec6de3f8e4d2bdebc461f3c30db7d3361
Paul Tunison <paul.tunison@kitware.com>
2015-09-02 17:21:02 -0400

    Removed use of pushd/popd from test runner

--- run_tests.sh
+++ run_tests.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env sh
 script_dir="$(cd "$(dirname "${BASE_SOURCE[0]}")" && pwd)"
-pushd ${script_dir}
+cd "${script_dir}"
 if [ -f ".coverage" ]
 then
   echo "Removing previous coverage cache file"
   rm ".coverage"
 fi
 nosetests --with-doctest --with-coverage --cover-package=smqtk --exclude-dir-file=nose_exclude_dirs.txt python/smqtk
-popd

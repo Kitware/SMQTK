d5ff8cc60eb1e695080b5842023a3e02449c8f3e
Paul Tunison <paul.tunison@kitware.com>
2015-09-20 21:40:38 -0400

    added more basic tests for libSVM iqr index impl

--- python/smqtk/tests/iqr_index/test_iqr_svm_hik.py
+++ python/smqtk/tests/iqr_index/test_iqr_svm_hik.py
@@ -1,11 +1,36 @@
 import unittest
 
 import nose.tools as ntools
+import numpy as np
 
+from smqtk.data_rep.descriptor_element_impl.local_elements import \
+    DescriptorMemoryElement
 from smqtk.iqr_index.libsvm_hik import LibSvmHikIqrIndex
 
 
 __author__ = 'purg'
+
+
+d0 = DescriptorMemoryElement('index', 0)
+d0.set_vector(np.array([1, 0, 0, 0, 0], float))
+d1 = DescriptorMemoryElement('index', 1)
+d1.set_vector(np.array([0, 1, 0, 0, 0], float))
+d2 = DescriptorMemoryElement('index', 2)
+d2.set_vector(np.array([0, 0, 1, 0, 0], float))
+d3 = DescriptorMemoryElement('index', 3)
+d3.set_vector(np.array([0, 0, 0, 1, 0], float))
+d4 = DescriptorMemoryElement('index', 4)
+d4.set_vector(np.array([0, 0, 0, 0, 1], float))
+d5 = DescriptorMemoryElement('index', 5)
+d5.set_vector(np.array([0.5, 0, 0.5, 0, 0], float))
+d6 = DescriptorMemoryElement('index', 6)
+d6.set_vector(np.array([.2, .2, .2, .2, .2], float))
+index_descriptors = [d0, d1, d2, d3, d4, d5, d6]
+
+q_pos = DescriptorMemoryElement('query', 0)
+q_pos.set_vector(np.array([.75, .25, 0, 0, 0], float))
+q_neg = DescriptorMemoryElement('query', 1)
+q_neg.set_vector(np.array([0, 0, 0, .5, .5], float))
 
 
 class TestIqrSvmHik (unittest.TestCase):
@@ -23,3 +48,24 @@ class TestIqrSvmHik (unittest.TestCase):
 
         # test config idempotency
         ntools.assert_dict_equal(c, iqr_index.get_config())
+
+    def test_rank_no_neg(self):
+        iqr_index = LibSvmHikIqrIndex()
+        iqr_index.build_index(index_descriptors)
+        ntools.assert_raises(ValueError, iqr_index.rank, [q_pos])
+
+    def test_rank_no_pos(self):
+        iqr_index = LibSvmHikIqrIndex()
+        iqr_index.build_index(index_descriptors)
+        ntools.assert_raises(ValueError, iqr_index.rank, [], [q_neg])
+
+    def test_rank_no_input(self):
+        iqr_index = LibSvmHikIqrIndex()
+        iqr_index.build_index(index_descriptors)
+        ntools.assert_raises(ValueError, iqr_index.rank, [])
+
+    def test_count(self):
+        iqr_index = LibSvmHikIqrIndex()
+        ntools.assert_equal(iqr_index.count(), 0)
+        iqr_index.build_index(index_descriptors)
+        ntools.assert_equal(iqr_index.count(), 7)

8520e48559ab0d70e86c2e1ca69b5e639ffc9f86
Paul Tunison <paul.tunison@kitware.com>
2015-09-22 13:53:31 -0400

    Added a JSON configuration dictionary merge utility

---
+++ python/smqtk/utils/configuration.py
@@ -0,0 +1,17 @@
+__author__ = 'purg'
+
+
+def merge_configs(a, b):
+    """
+    Merge configuration b into configuration a. Congruent keys are overwritten,
+    while new keys are added.
+
+    This is different than normal dictionary update in that we don't bash
+    nested dictionaries, instead recursively updating them.
+
+    """
+    for k in b:
+        if k in a and isinstance(a[k], dict) and isinstance(b[k], dict):
+            merge_configs(a[k], b[k])
+        else:
+            a[k] = b[k]

4f0b74eb67bed70917a4cf6d88fe409f43f0ef4f
Paul Tunison <paul.tunison@kitware.com>
2015-10-08 09:30:23 +0900

    fixed safe_create_dir test mock calls

--- python/smqtk/tests/utils/test_safe_create_dir.py
+++ python/smqtk/tests/utils/test_safe_create_dir.py
@@ -11,7 +11,7 @@ from smqtk.utils import file_utils
 
 class TestSafeCreateDir (unittest.TestCase):
 
-    @mock.patch('smqtk.utils.os.makedirs')
+    @mock.patch('smqtk.utils.file_utils.os.makedirs')
     def test_noExists(self, mock_os_makedirs):
         dir_path = "/some/directory/somewhere"
         p = file_utils.safe_create_dir(dir_path)
@@ -19,8 +19,8 @@ class TestSafeCreateDir (unittest.TestCase):
         ntools.assert_true(mock_os_makedirs.called)
         ntools.assert_equals(p, dir_path)
 
-    @mock.patch('smqtk.utils.os.path.exists')
-    @mock.patch('smqtk.utils.os.makedirs')
+    @mock.patch('smqtk.utils.file_utils.os.path.exists')
+    @mock.patch('smqtk.utils.file_utils.os.makedirs')
     def test_existError_alreadyExists(self, mock_os_makedirs, mock_osp_exists):
         mock_os_makedirs.side_effect = OSError(errno.EEXIST,
                                                "Existing directory")
@@ -35,8 +35,8 @@ class TestSafeCreateDir (unittest.TestCase):
         mock_osp_exists.assert_called_once_with(dir_path)
         ntools.assert_equal(p, dir_path)
 
-    @mock.patch('smqtk.utils.os.path.exists')
-    @mock.patch('smqtk.utils.os.makedirs')
+    @mock.patch('smqtk.utils.file_utils.os.path.exists')
+    @mock.patch('smqtk.utils.file_utils.os.makedirs')
     def test_existError_noExist(self, mock_os_makedirs, mock_osp_exists):
         mock_os_makedirs.side_effect = OSError(errno.EEXIST,
                                                "Existing directory")
@@ -48,8 +48,8 @@ class TestSafeCreateDir (unittest.TestCase):
         mock_os_makedirs.assert_called_once_with(dir_path)
         mock_osp_exists.assert_called_once_with(dir_path)
 
-    @mock.patch('smqtk.utils.os.path.exists')
-    @mock.patch('smqtk.utils.os.makedirs')
+    @mock.patch('smqtk.utils.file_utils.os.path.exists')
+    @mock.patch('smqtk.utils.file_utils.os.makedirs')
     def test_otherOsError(self, mock_os_makedirs, mock_osp_exists):
         mock_os_makedirs.side_effect = OSError(errno.EACCES,
                                                "Permission Denied")
@@ -60,7 +60,7 @@ class TestSafeCreateDir (unittest.TestCase):
         mock_os_makedirs.assert_called_once_with(dir_path)
         ntools.assert_false(mock_osp_exists.called)
 
-    @mock.patch('smqtk.utils.os.makedirs')
+    @mock.patch('smqtk.utils.file_utils.os.makedirs')
     def test_otherException(self, mock_os_makedirs):
         mock_os_makedirs.side_effect = RuntimeError("Some other exception")
 

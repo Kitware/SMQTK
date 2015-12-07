0273b18f9a4535f4f76ff6c66bbe72b61313bce0
Paul Tunison <paul.tunison@kitware.com>
2015-12-07 10:28:39 -0500

    Protected ColorDescriptor test behind `is_usable` test

--- python/smqtk/tests/algorithms/content_description/test_colordescriptor.py
+++ python/smqtk/tests/algorithms/content_description/test_colordescriptor.py
@@ -10,29 +10,32 @@ from smqtk.algorithms.descriptor_generator.colordescriptor.colordescriptor impor
 __author__ = "paul.tunison@kitware.com"
 
 
-class TestColorDescriptor (unittest.TestCase):
+if ColorDescriptor_Image_csift.is_usable():
 
-    @mock.patch('smqtk.algorithms.descriptor_generator.colordescriptor.colordescriptor'
-                '.file_utils.safe_create_dir')
-    def test_configuration(self, mock_scd):
-        default_config = ColorDescriptor_Image_csift.get_default_config()
-        default_config['model_directory'] = '/some/path/models/'
-        default_config['work_directory'] = '/some/path/work/'
+    class TestColorDescriptor (unittest.TestCase):
 
-        inst = ColorDescriptor_Image_csift.from_config(default_config)
-        ntools.assert_equal(
-            default_config,
-            inst.get_config()
-        )
-        inst2 = ColorDescriptor_Image_csift.from_config(inst.get_config())
-        ntools.assert_equal(inst.get_config(), inst2.get_config())
-        ntools.assert_equal(inst._model_dir, inst2._model_dir)
-        ntools.assert_equal(inst._work_dir, inst2._work_dir)
-        ntools.assert_equal(inst._kmeans_k, inst2._kmeans_k)
-        ntools.assert_equal(inst._flann_target_precision,
-                            inst2._flann_target_precision)
-        ntools.assert_equal(inst._flann_sample_fraction,
-                            inst2._flann_sample_fraction)
-        ntools.assert_equal(inst._flann_autotune, inst2._flann_autotune)
-        ntools.assert_equal(inst._use_sp, inst2._use_sp)
-        ntools.assert_equal(inst._rand_seed, inst2._rand_seed)
+        @mock.patch('smqtk.algorithms.descriptor_generator'
+                    '.colordescriptor.colordescriptor'
+                    '.file_utils.safe_create_dir')
+        def test_configuration(self, mock_scd):
+            default_config = ColorDescriptor_Image_csift.get_default_config()
+            default_config['model_directory'] = '/some/path/models/'
+            default_config['work_directory'] = '/some/path/work/'
+
+            inst = ColorDescriptor_Image_csift.from_config(default_config)
+            ntools.assert_equal(
+                default_config,
+                inst.get_config()
+            )
+            inst2 = ColorDescriptor_Image_csift.from_config(inst.get_config())
+            ntools.assert_equal(inst.get_config(), inst2.get_config())
+            ntools.assert_equal(inst._model_dir, inst2._model_dir)
+            ntools.assert_equal(inst._work_dir, inst2._work_dir)
+            ntools.assert_equal(inst._kmeans_k, inst2._kmeans_k)
+            ntools.assert_equal(inst._flann_target_precision,
+                                inst2._flann_target_precision)
+            ntools.assert_equal(inst._flann_sample_fraction,
+                                inst2._flann_sample_fraction)
+            ntools.assert_equal(inst._flann_autotune, inst2._flann_autotune)
+            ntools.assert_equal(inst._use_sp, inst2._use_sp)
+            ntools.assert_equal(inst._rand_seed, inst2._rand_seed)

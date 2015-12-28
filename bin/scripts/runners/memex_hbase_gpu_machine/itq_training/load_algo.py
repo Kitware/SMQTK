76e80d9c48008a7ee6410153840ab0f69949e213
Paul Tunison <paul.tunison@kitware.com>
2015-12-28 17:58:34 -0500

    Generalized a little and tweaked ITQ train scripts for local use

--- bin/scripts/runners/memex_hbase_gpu_machine/itq_training/load_algo.py
+++ bin/scripts/runners/memex_hbase_gpu_machine/itq_training/load_algo.py
@@ -2,13 +2,11 @@ import json
 import os
 
 import smqtk.algorithms.nn_index.lsh.itq
-
-
-THIS_DIR = os.path.dirname(__file__)
+import smqtk.utils.jsmin as jsmin
 
 
 def load_algo(m=smqtk.algorithms.nn_index.lsh.itq):
-    with open(os.path.join(THIS_DIR, "itq_config.json")) as f:
-        itq_config = json.load(f)
+    with open("itq_config.json") as f:
+        itq_config = json.loads(jsmin.jsmin(f.read()))
     itq_index = m.ITQNearestNeighborsIndex.from_config(itq_config)
     return itq_index
--- bin/scripts/runners/memex_hbase_gpu_machine/itq_training/train_model.py
+++ bin/scripts/runners/memex_hbase_gpu_machine/itq_training/train_model.py
@@ -1,23 +1,32 @@
+import json
+
+from smqtk.representation import DescriptorElementFactory
 from smqtk.utils.bin_utils import logging, initialize_logging
+from smqtk.utils.jsmin import jsmin
+
+from load_algo import load_algo
+
+
 if not logging.getLogger().handlers:
     initialize_logging(logging.getLogger(), logging.DEBUG)
 log = logging.getLogger(__name__)
 
+
 log.info("Loading descriptor elements")
-import cPickle
-import json
-from smqtk.representation.descriptor_element.postgres_element import PostgresDescriptorElement
-d_config = json.load(open('/data/kitware/smqtk/image_cache_cnn_compute/psql_descriptor_config.localhost.json'))
-d_type_str = open("/data/kitware/smqtk/image_cache_cnn_compute/descriptor_type_name.txt").read().strip()
+df_config = json.loads(jsmin(open('descriptor_factory_config.json').read()))
+factory = DescriptorElementFactory.from_config(df_config)
+
+d_type_str = open("descriptor_type_name.txt").read().strip()
 d_elements = []
-with open("/data/kitware/smqtk/image_cache_cnn_compute/descriptor_uuid_set.1m_train_sample.pickle") as f:
-    for uuid in cPickle.load(f):
-        d_elements.append(
-            PostgresDescriptorElement.from_config(d_config, d_type_str, uuid)
-        )
+with open("descriptor_uuids.train.txt") as f:
+    for uuid in (l.strip() for l in f):
+        d_elements.append(factory(d_type_str, uuid))
+
+log.info("Sorting descriptors by UUID")
+d_elements.sort(key=lambda e: e.uuid())
+
 
 log.info("Loading ITQ index algo")
-from load_algo import load_algo
 itq_index = load_algo()
 
 log.info("Building index")

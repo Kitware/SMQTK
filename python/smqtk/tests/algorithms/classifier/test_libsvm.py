1cc181a7f3a4f14d3416c3407d42d1ffba78dd6b
Paul Tunison <paul.tunison@kitware.com>
2016-01-05 15:41:00 -0500

    Added libSVM multiclass test

--- python/smqtk/tests/algorithms/classifier/test_libsvm.py
+++ python/smqtk/tests/algorithms/classifier/test_libsvm.py
@@ -80,3 +80,87 @@ if LibSvmClassifier.is_usable():
             # Closing resources
             p.close()
             p.join()
+
+        def test_simple_multiclass_classification(self):
+            """
+            Test libSVM classification functionality using random constructed
+            data, training the y=0.33 and y=.66 split
+            """
+            DIM = 2
+            N = 1000
+            P1_LABEL = 'p1'
+            P2_LABEL = 'p2'
+            p = multiprocessing.pool.ThreadPool()
+            d_factory = DescriptorElementFactory(DescriptorMemoryElement, {})
+            c_factory = ClassificationElementFactory(MemoryClassificationElement, {})
+            di = 0
+
+            def make_element((i, v)):
+                d = d_factory.new_descriptor('test', i)
+                d.set_vector(v)
+                return d
+
+            # Constructing artificial descriptors
+            x = numpy.random.rand(N, DIM)
+            x_p1 = x[x[:, 1] <= 0.30]
+            x_p2 = x[(x[:, 1] >= 0.36) & (x[:, 1] <= 0.63)]
+            x_neg = x[x[:, 1] >= 0.69]
+
+            d_p1 = p.map(make_element, enumerate(x_p1, di))
+            di += len(d_p1)
+            d_p2 = p.map(make_element, enumerate(x_p2, di))
+            di += len(d_p2)
+            d_neg = p.map(make_element, enumerate(x_neg, di))
+            di += len(d_neg)
+
+            # Create/Train test classifier
+            classifier = LibSvmClassifier(
+                train_params={
+                    '-t': 0,  # linear kernel
+                    '-b': 1,  # enable probability estimates
+                    '-c': 2,  # SVM-C parameter C
+                    '-q': ''  # quite mode
+                },
+                normalize=None,  # DO NOT normalize descriptors
+            )
+            classifier.train({P1_LABEL: d_p1, P2_LABEL: d_p2}, d_neg)
+
+            # Test classifier
+            x = numpy.random.rand(N, DIM)
+            x_p1 = x[x[:, 1] <= 0.30]
+            x_p2 = x[(x[:, 1] >= 0.36) & (x[:, 1] <= 0.63)]
+            x_neg = x[x[:, 1] >= 0.69]
+
+            d_p1 = p.map(make_element, enumerate(x_p1, di))
+            di += len(d_p1)
+            d_p2 = p.map(make_element, enumerate(x_p2, di))
+            di += len(d_p2)
+            d_neg = p.map(make_element, enumerate(x_neg, di))
+            di += len(d_neg)
+
+            for d in d_p1:
+                c = classifier.classify(d, c_factory)
+                ntools.assert_equal(c.max_label(),
+                                    P1_LABEL,
+                                    "Incorrect %s label: %s :: %s" %
+                                    (P1_LABEL, d.vector(),
+                                     c.get_classification()))
+            for d in d_p2:
+                c = classifier.classify(d, c_factory)
+                ntools.assert_equal(c.max_label(),
+                                    P2_LABEL,
+                                    "Incorrect %s label: %s :: %s" %
+                                    (P2_LABEL, d.vector(),
+                                     c.get_classification()))
+            for d in d_neg:
+                c = classifier.classify(d, c_factory)
+                ntools.assert_equal(c.max_label(),
+                                    LibSvmClassifier.NEGATIVE_LABEL,
+                                    "Incorrect %s label: %s :: %s" %
+                                    (LibSvmClassifier.NEGATIVE_LABEL,
+                                     d.vector(),
+                                     c.get_classification()))
+
+            # Closing resources
+            p.close()
+            p.join()

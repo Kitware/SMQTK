84a81aeaf8ec09d99c8493a5f13e4799bb35e112
Paul Tunison <paul.tunison@kitware.com>
2016-01-15 18:22:32 -0500

    moved itq training example scripts up a level

--- bin/scripts/runners/itq_training/load_algo.py
+++ bin/scripts/runners/itq_training/load_algo.py
@@ -1,11 +1,10 @@
 import json
-import os
 
-import smqtk.algorithms.nn_index.lsh.itq
+import smqtk.algorithms.nn_index.lsh.functors.itq
 import smqtk.utils.jsmin as jsmin
 
 
-def load_algo(m=smqtk.algorithms.nn_index.lsh.itq):
+def load_algo(m=smqtk.algorithms.nn_index.lsh.functors.itq):
     with open("itq_config.json") as f:
         itq_config = json.loads(jsmin.jsmin(f.read()))
     itq_index = m.ITQNearestNeighborsIndex.from_config(itq_config)

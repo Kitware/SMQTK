d2bbbd4079bc7a19e9afb95eadf91b4b76b1614c
Paul Tunison <paul.tunison@kitware.com>
2016-03-30 15:38:54 -0400

    Updated watch script for CDR fetch with new downloaded reporting

--- bin/memex/watch_cdr_get.sh
+++ bin/memex/watch_cdr_get.sh
@@ -6,11 +6,13 @@ log_file="test_output.log"
 interval=1  # seconds
 
 watch -n ${interval} "
+    # This first one will be expensive for large archives
+    #echo \"All Files Downloaded            : \$(find \"${output_dir}/\" -type f | wc -l)\";
     echo \"CDR records scanned             : \$(grep \"report_progress\" \"${log_file}\" | tail -1 | cut -d' ' -f20)\";
-    echo \"Files Downloaded                : \$(find \"${output_dir}/\" -type f | wc -l)\";
-    echo \"Files Recorded                  : \$(cat \"${output_csv}\" | wc -l)\";
     echo \"Download stored-data failures   : \$(grep \"stored-data URL\" \"${log_file}\" | wc -l)\";
     echo \"Download fallback failures      : \$(grep \"original URL\" \"${log_file}\" | wc -l)\";
     echo \"DL Failures caused by exceptions: \$(grep \"\(Exception\|Error\|FAILED\)\" \"${log_file}\" | wc -l)\"
+    echo \"Files Recorded                  : \$(cat \"${output_csv}\" | wc -l)\";
+    echo \"Files downloaded                : \$(grep \"Saving to file\" \"${log_file}\" | wc -l)\";
     echo \"Scan Restarts                   : \$(grep \"Restarting query\" \"${log_file}\" | wc -l)\"
 "

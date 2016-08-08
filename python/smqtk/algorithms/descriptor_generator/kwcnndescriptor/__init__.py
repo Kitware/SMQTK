dd82a3e4aedf215f27801227408d6bc35d0063e8
Paul Tunison <paul.tunison@kitware.com>
2016-08-08 17:17:12 -0400

    Fix typo

--- python/smqtk/algorithms/descriptor_generator/kwcnndescriptor/__init__.py
+++ python/smqtk/algorithms/descriptor_generator/kwcnndescriptor/__init__.py
@@ -1,7 +1,7 @@
 from .kwcnndescriptor import KWCNNDescriptorGenerator
 
 
-__author__ = 'jason.parham@kiwrare.com,paul.tunison@kitware.com'
+__author__ = 'jason.parham@kitware.com,paul.tunison@kitware.com'
 
 
 DESCRIPTOR_GENERATOR_CLASS = KWCNNDescriptorGenerator

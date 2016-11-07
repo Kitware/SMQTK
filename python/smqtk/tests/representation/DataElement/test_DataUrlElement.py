6346c969533137ff15e976c537397e57e971d726
Paul Tunison <paul.tunison@kitware.com>
2016-11-07 12:44:44 -0500

    Updated existing URL element tests

--- python/smqtk/tests/representation/DataElement/test_DataUrlElement.py
+++ python/smqtk/tests/representation/DataElement/test_DataUrlElement.py
@@ -1,29 +1,26 @@
-
 import nose.tools as ntools
+import os
 import requests
 import unittest
 
 from smqtk.representation.data_element.url_element import DataUrlElement
+from smqtk.tests import TEST_DATA_DIR
 
 
-__author__ = "paul.tunison@kitware.com"
-
-
-# Check to see if we have an internet connection (by checking github.com
-# website resolution).
+# Check to see if we have an internet connection.
 internet_available = True
 try:
-    r = requests.get('http://github.com')
+    r = requests.get('https://data.kitware.com')
     _ = r.content
-except Exception, ex:
+except requests.ConnectionError:
     internet_available = False
 
 
 if internet_available:
 
-    # Public domain rose image
-    EXAMPLE_URL = \
-        'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png'
+    # Public domain Lenna image from Wikipedia, same as local test image file
+    EXAMPLE_URL = 'https://data.kitware.com/api/v1/file/5820bbeb8d777f10f26efc2f/download'
+    EXAMPLE_PTH = os.path.join(TEST_DATA_DIR, 'Lenna.png')
 
 
     class TestDataUrlElement (unittest.TestCase):
@@ -33,7 +30,15 @@ if internet_available:
 
         """
 
-        def test_configuration(self):
+        def test_new(self):
+            e = DataUrlElement(EXAMPLE_URL)
+            ntools.assert_equal(e.get_bytes(), open(EXAMPLE_PTH).read())
+
+        def test_content_type(self):
+            e = DataUrlElement(EXAMPLE_URL)
+            ntools.assert_equal(e.content_type(), 'image/png')
+
+        def test_from_configuration(self):
             default_config = DataUrlElement.get_default_config()
             ntools.assert_equal(default_config, {'url_address': None})
 
@@ -41,6 +46,7 @@ if internet_available:
             inst1 = DataUrlElement.from_config(default_config)
             ntools.assert_equal(default_config, inst1.get_config())
             ntools.assert_equal(inst1._url, EXAMPLE_URL)
+            ntools.assert_equal(inst1.get_bytes(), open(EXAMPLE_PTH).read())
 
             inst2 = DataUrlElement.from_config(inst1.get_config())
             ntools.assert_equal(inst1, inst2)

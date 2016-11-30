00ea90d11ecee1392e1f625ecc7e821a75611688
Dan LaManna <dan.lamanna@kitware.com>
2016-11-30 10:39:48 -0500

    Add testing for cli interface to smqtk-check-images

--- python/smqtk/tests/utils/test_image_utils.py
+++ python/smqtk/tests/utils/test_image_utils.py
@@ -1,8 +1,13 @@
+import mock
 import os
+import sys
+import tempfile
 import unittest
 
 import nose.tools as ntools
+from StringIO import StringIO
 
+from smqtk.bin.check_images import main as check_images_main
 from smqtk.representation.data_element.file_element import DataFileElement
 from smqtk.tests import TEST_DATA_DIR
 from smqtk.utils.image_utils import is_loadable_image, is_valid_element
@@ -59,3 +64,52 @@ class TestIsValidElement(unittest.TestCase):
 
     def test_invalid_image_returns_false(self):
         assert is_valid_element(self.non_image, check_image=True) == False
+
+
+class TestCheckImageCli(unittest.TestCase):
+
+    def check_images(self):
+        stdout, stderr = False, False
+        saved_stdout, saved_stderr = sys.stdout, sys.stderr
+
+        try:
+            out, err = StringIO(), StringIO()
+            sys.stdout, sys.stderr = out, err
+            check_images_main()
+        except SystemExit:
+            pass
+        finally:
+            stdout, stderr = out.getvalue().strip(), err.getvalue().strip()
+            sys.stdout, sys.stderr = saved_stdout, saved_stderr
+
+        return stdout, stderr
+
+
+    def test_base_case(self):
+        with mock.patch.object(sys, 'argv', ['']):
+            assert 'Validate a list of images returning the filepaths' in \
+                self.check_images()[0]
+
+
+    def test_check_images(self):
+        # Create test file with a valid, invalid, and non-existent image
+        _, filename = tempfile.mkstemp()
+
+        with open(filename, 'w') as outfile:
+            outfile.write(os.path.join(TEST_DATA_DIR, 'Lenna.png') + '\n')
+            outfile.write(os.path.join(TEST_DATA_DIR, 'test_file.dat') + '\n')
+            outfile.write(os.path.join(TEST_DATA_DIR, 'non-existent-file.jpeg'))
+
+        with mock.patch.object(sys, 'argv', ['', '--file-list', filename]):
+            out, err = self.check_images()
+
+            assert out == ','.join([os.path.join(TEST_DATA_DIR, 'Lenna.png'),
+                                    '3ee0d360dc12003c0d43e3579295b52b64906e85'])
+            assert 'non-existent-file.jpeg' not in out
+
+        with mock.patch.object(sys, 'argv', ['', '--file-list', filename, '--invert']):
+            out, err = self.check_images()
+
+            assert out == ','.join([os.path.join(TEST_DATA_DIR, 'test_file.dat'),
+                                    'da39a3ee5e6b4b0d3255bfef95601890afd80709'])
+            assert 'non-existent-file.jpeg' not in out

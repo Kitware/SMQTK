1bf040571f26a886b9233a7c29e84d6681c69da8
Paul Tunison <paul.tunison@kitware.com>
2016-12-27 16:34:46 -0500

    Add global tests for HBase element, add missing test file

--- python/smqtk/tests/representation/DataElement/test_HBaseDataElement.py
+++ python/smqtk/tests/representation/DataElement/test_HBaseDataElement.py
@@ -7,54 +7,72 @@ import smqtk.exceptions
 from smqtk.representation.data_element.hbase_element import HBaseDataElement
 
 
-if HBaseDataElement.is_usable():
+class TestHBaseDataElement(TestCase):
 
-    class TestHBaseDataElement(TestCase):
+    DUMMY_CFG = {
+        'element_key': 'foobar',
+        'binary_column': 'binary_data',
+        'hbase_address': 'some_address',
+        'hbase_table': 'some_table',
+        'timeout': 12345,
+    }
 
-        DUMMY_CFG = {
-            'element_key': 'foobar',
-            'binary_column': 'binary_data',
-            'hbase_address': 'some_address',
-            'hbase_table': 'some_table',
-            'timeout': 12345,
+    def make_test_element(self, content):
+        e = HBaseDataElement(**self.DUMMY_CFG)
+        # Pretend that the implementation is actually available and mock out
+        # dependency functionality.
+        e.content_type = mock.MagicMock()
+        e._new_hbase_table_connection = mock.MagicMock()
+        e._new_hbase_table_connection().row.return_value = {
+            self.DUMMY_CFG['binary_column']: content
         }
+        return e
 
-        def test_config(self):
-            cfg = self.DUMMY_CFG
+    @classmethod
+    def setUpClass(cls):
+        # Pretend that the implementation is actually available and mock out
+        # dependency functionality.
+        HBaseDataElement.is_usable = mock.MagicMock(return_value=True)
 
-            e = HBaseDataElement.from_config(cfg)
+    def test_config(self):
+        cfg = self.DUMMY_CFG
 
-            nose.tools.assert_equal(e.element_key, cfg['element_key'])
-            nose.tools.assert_equal(e.binary_column, cfg['binary_column'])
-            nose.tools.assert_equal(e.hbase_address, cfg['hbase_address'])
-            nose.tools.assert_equal(e.hbase_table, cfg['hbase_table'])
-            nose.tools.assert_equal(e.timeout, cfg['timeout'])
+        e = HBaseDataElement.from_config(cfg)
 
-            # output should be the same as what we constructed with in this case
-            e_get_cfg = e.get_config()
-            nose.tools.assert_equal(e_get_cfg, cfg)
+        nose.tools.assert_equal(e.element_key, cfg['element_key'])
+        nose.tools.assert_equal(e.binary_column, cfg['binary_column'])
+        nose.tools.assert_equal(e.hbase_address, cfg['hbase_address'])
+        nose.tools.assert_equal(e.hbase_table, cfg['hbase_table'])
+        nose.tools.assert_equal(e.timeout, cfg['timeout'])
 
-        def test_is_empty_zero_bytes(self):
-            e = HBaseDataElement(**self.DUMMY_CFG)
-            # Simulate empty bytes
-            e.get_bytes = mock.MagicMock(return_value='')
-            nose.tools.assert_true(e.is_empty())
+        # output should be the same as what we constructed with in this case
+        e_get_cfg = e.get_config()
+        nose.tools.assert_equal(e_get_cfg, cfg)
 
-        def test_is_empty_nonzero_bytes(self):
-            e = HBaseDataElement(**self.DUMMY_CFG)
-            # Simulate non-empty bytes
-            e.get_bytes = mock.MagicMock(return_value='some bytes')
-            nose.tools.assert_false(e.is_empty())
+    def test_get_bytes(self):
+        expected_bytes = 'foo bar test string'
+        e = self.make_test_element(expected_bytes)
+        nose.tools.assert_equal(e.get_bytes(), expected_bytes)
 
-        def test_writable(self):
-            # Read-only element
-            e = HBaseDataElement(**self.DUMMY_CFG)
-            nose.tools.assert_false(e.writable())
+    def test_is_empty_zero_bytes(self):
+        # Simulate empty bytes
+        e = self.make_test_element('')
+        nose.tools.assert_true(e.is_empty())
 
-        def test_set_bytes(self):
-            # Read-only element
-            e = HBaseDataElement(**self.DUMMY_CFG)
-            nose.tools.assert_raises(
-                smqtk.exceptions.ReadOnlyError,
-                e.set_bytes, 'some bytes'
-            )
+    def test_is_empty_nonzero_bytes(self):
+        # Simulate non-empty bytes
+        e = self.make_test_element('some bytes')
+        nose.tools.assert_false(e.is_empty())
+
+    def test_writable(self):
+        # Read-only element
+        e = self.make_test_element('')
+        nose.tools.assert_false(e.writable())
+
+    def test_set_bytes(self):
+        # Read-only element
+        e = self.make_test_element('')
+        nose.tools.assert_raises(
+            smqtk.exceptions.ReadOnlyError,
+            e.set_bytes, 'some bytes'
+        )

327d49b03c0fd38221bc0714e5caf7c452cfe25a
Jonathan Owens <jonathan.owens@kitware.com>
2017-01-05 10:57:49 -0500

    Complete from_uri method in GirderDataElement, as well as add tests.

--- python/smqtk/representation/data_element/girder.py
+++ python/smqtk/representation/data_element/girder.py
@@ -1,3 +1,4 @@
+from urlparse import urlparse
 import requests
 
 from smqtk.exceptions import ReadOnlyError
@@ -28,12 +29,96 @@ class GirderDataElement (DataElement):
         return True
 
     # TODO: from_uri
-    #       - adapt http format for username/password specification
-    #           (i.e. girder://<user>:<pass>@<url...>
     #       - maybe optionally allow API key in place of user/pass spec
     #           (i.e. girder://<api_key>@<url...>
     #       - <url> in above I guess would be the api/v1/... URL, including any
     #           parameters needed
+    @classmethod
+    def from_uri(cls, uri):
+        """
+        Creates a ``GirderDataElement`` from ``uri``.
+
+        The parsing is accomplished by passing ``uri`` to ``urlparse``.
+        This allows for quite a bit of flexibility in the types of URIs that
+        can be passed. Valid Girder URIs are:
+
+        (a) girder(s)://<user>:<pass>@<host>:<port>/api/v1/file/<file_id>
+        (b) girder://<user>:<pass>@file:<file_id>
+
+        <user> and <pass> are optional in both (a) and (b). <port> is optional
+        in (a).
+
+        If the (b) form of a Girder URI is used, then the ``api_root`` member
+        will be the default of 'http://localhost:8080/api/v1'.
+
+        Currently, only GirderDataElements can be built from Files, so the
+        URL should end in /file/{id}.
+
+        :param uri: A URI of the form
+        girder(s)://<user>:<pass>@<host>:<port>/api/<version>/file/<file_id> or
+        girder://<user>:<pass>@file:<file_id>.
+        :type uri: str
+
+        :return: Data element created from ``uri``
+        :rtype: GirderDataElement
+
+        :raises ValueError: An invalid URI was passed.
+
+        :raises AssertionError: If the path parsed from a URI of the form (a)
+        does not have '/file/' as its penultimate location before the
+        identifier.
+        """
+        api_root = None
+        # urlparse seems to not be phased by the 'girder' protocol instead of
+        # http, so no replacing needs to be done.
+        parsed_uri = urlparse(uri)
+        if not parsed_uri.scheme.startswith('girder'):
+            raise ValueError('Invalid Girder URI. Girder URIs must start with '
+                             'girder:// or girders://')
+
+        # For the API root to be valid, the URI must have a netloc
+        # and the ``path`` must start with '/api'. This clause deals with
+        # URIs of the form:
+        # girder://<user>:<pass>@<host>:<port>/api/v1/file/<file_id>
+        # The logic is constructed by understanding how urlparse parses a
+        # URI of the above form. <port> is optional
+        if parsed_uri.path.startswith('/api') and parsed_uri.netloc:
+            assert parsed_uri.path.split('/file/')[0] != parsed_uri.path
+            # If you're passing a URI of the form (a), either <girder> or
+            # <girders> are valid tags. We determine the scheme to use in
+            # constructing the api_root here based on that tag.
+            if parsed_uri.scheme == 'girder':
+                api_root = 'http://'
+            elif parsed_uri.scheme == 'girders':
+                api_root = 'https://'
+
+            # We rsplit on '/file/' to get the preceding path information
+            # before /file/<file_id>, which is used to construct the api root.
+            api_root += '%s%s' % (parsed_uri.netloc,
+                                  parsed_uri.path.rsplit('/file/')[0])
+            file_id = parsed_uri.path.split('/')[-1]
+
+        # This covers the case of a URI of the form:
+        # girder://<user>:<pass>@file:<file_id>
+        elif parsed_uri.netloc.startswith('file'):
+            file_id = parsed_uri.netloc.split(':')[-1]
+
+        # The above are the two currently supported forms of a Girder URI in
+        # SMQTK. Anything else will be considered invalid.
+        else:
+            raise ValueError(
+                'Invalid Girder URI. Girder URIs must be of the form: \n'
+                '* girder://<user>:<pass>@<host>:<port>/api/<version>/'
+                'file/<file_id>\n'
+                '* girder://<user>:<pass>@file:<file_id>\n'
+                'Where <user> and <pass> are optional.'
+            )
+
+        if api_root:
+            girder_element = cls(file_id, api_root)
+        else:
+            girder_element = cls(file_id)
+        return girder_element
 
     def __init__(self, file_id, api_root='http://localhost:8080/api/v1',
                  api_key=None):
@@ -59,6 +144,9 @@ class GirderDataElement (DataElement):
 
         self.file_id = file_id
         self.api_root = api_root
+        # TODO: Should token manager become a property so that if ``api_key``
+        # is set after-the-fact the GirderTokenManager is instantiated
+        # properly?
         self.token_manager = GirderTokenManager(api_root, api_key)
 
         self.token = None

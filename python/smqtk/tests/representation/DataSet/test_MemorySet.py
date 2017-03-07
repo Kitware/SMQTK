3f59a54063641c4be135227d7c0a3930ce37d8b2
Paul Tunison <paul.tunison@kitware.com>
2017-03-07 10:08:21 -0500

    Update DataMemorySet to use cache element instead of file

--- python/smqtk/tests/representation/DataSet/test_MemorySet.py
+++ python/smqtk/tests/representation/DataSet/test_MemorySet.py
@@ -1,47 +1,84 @@
-import mock
 import nose.tools as ntools
+import pickle
 import unittest
 
+from smqtk.exceptions import ReadOnlyError
 from smqtk.representation.data_element.memory_element import DataMemoryElement
 from smqtk.representation.data_set.memory_set import DataMemorySet
 
 
 class TestDataFileSet (unittest.TestCase):
 
-    def test_configuration(self):
+    def test_is_usable(self):
+        # no dependencies
+        ntools.assert_true(DataMemorySet.is_usable())
+
+    def test_default_config(self):
         default_config = DataMemorySet.get_default_config()
-        expected_config = {
-            "file_cache": None,
-            "pickle_protocol": -1,
-        }
-        ntools.assert_equal(default_config, expected_config)
+        ntools.assert_equal(len(default_config), 2)
+        ntools.assert_in('cache_element', default_config)
+        ntools.assert_is_instance(default_config['cache_element'],
+                                  dict)
+        ntools.assert_is_none(default_config['cache_element']['type'])
+        ntools.assert_in('pickle_protocol', default_config)
 
-        inst1 = DataMemorySet.from_config(default_config)
-        # idempotency
-        ntools.assert_equal(default_config, inst1.get_config())
+    def test_from_config_default(self):
+        # From default configuration, which should be valid. Specifies no cache
+        # pickle protocol -1.
+        c = DataMemorySet.get_default_config()
+        i = DataMemorySet.from_config(c)
+        ntools.assert_is_none(i.cache_element)
+        ntools.assert_equal(i.pickle_protocol, -1)
+        ntools.assert_equal(i._element_map, {})
 
-        inst2 = DataMemorySet.from_config(inst1.get_config())
-        ntools.assert_equal(inst1, inst2)
+    def test_from_config_empty_cache(self):
+        # Specify a memory element cache with no pre-existing bytes.
+        c = DataMemorySet.get_default_config()
+        c['cache_element']['type'] = 'DataMemoryElement'
+        i = DataMemorySet.from_config(c)
+        ntools.assert_is_not_none(i.cache_element)
+        ntools.assert_is_instance(i.cache_element, DataMemoryElement)
+        ntools.assert_equal(i.cache_element.get_bytes(), '')
+        ntools.assert_equal(i.pickle_protocol, -1)
+        ntools.assert_equal(i._element_map, {})
 
-    @mock.patch('smqtk.representation.data_set.memory_set.os.path.isfile')
-    @mock.patch('smqtk.representation.data_set.memory_set.pickle')
-    @mock.patch('__builtin__.open')
-    def test_new_from_cache(self, m_open, m_pickle, m_isfile):
-        expected_cache_path = '/some/file/path'
-        expected_pickle_load_return = {'key': 'value'}
+    def test_from_config_with_cache(self):
+        # Use a cache element with bytes defining pickle of map to use.
+        expected_map = dict(a=1, b=2, c=3)
 
-        # so os.path.isfile returns true for our mock path
-        def expected_isfile_effect(path):
-            if path == expected_cache_path:
-                return True
-            raise RuntimeError("Did not get expected path.")
+        c = DataMemorySet.get_default_config()
+        c['cache_element']['type'] = 'DataMemoryElement'
+        c['cache_element']['DataMemoryElement']['bytes'] = \
+            pickle.dumps(expected_map)
 
-        m_pickle.load.return_value = expected_pickle_load_return
-        m_isfile.side_effect = expected_isfile_effect
+        i = DataMemorySet.from_config(c)
 
-        dms = DataMemorySet(expected_cache_path)
-        m_open.assert_called_once_with(expected_cache_path)
-        ntools.assert_equal(dms._element_map, expected_pickle_load_return)
+        ntools.assert_is_instance(i.cache_element, DataMemoryElement)
+        ntools.assert_equal(i.pickle_protocol, -1)
+        ntools.assert_equal(i._element_map, expected_map)
+
+    def test_init_no_cache(self):
+        i = DataMemorySet()
+        ntools.assert_is_none(i.cache_element)
+        ntools.assert_equal(i._element_map, {})
+        ntools.assert_equal(i.pickle_protocol, -1)
+
+    def test_init_empty_cache(self):
+        cache_elem = DataMemoryElement()
+        i = DataMemorySet(cache_elem, 2)
+        ntools.assert_equal(i.cache_element, cache_elem)
+        ntools.assert_equal(i.pickle_protocol, 2)
+        ntools.assert_equal(i._element_map, {})
+
+    def test_init_with_cache(self):
+        expected_map = dict(a=1, b=2, c=3)
+        expected_cache = DataMemoryElement(bytes=pickle.dumps(expected_map))
+
+        i = DataMemorySet(expected_cache)
+
+        ntools.assert_equal(i.cache_element, expected_cache)
+        ntools.assert_equal(i.pickle_protocol, -1)
+        ntools.assert_equal(i._element_map, expected_map)
 
     def test_iter(self):
         expected_map = {
@@ -56,28 +93,60 @@ class TestDataFileSet (unittest.TestCase):
         ntools.assert_equal(set(dms), expected_map_values)
         ntools.assert_equal(set(iter(dms)), expected_map_values)
 
-    @mock.patch('smqtk.representation.data_set.memory_set.pickle')
-    @mock.patch('smqtk.representation.data_set.memory_set.safe_file_write')
-    def test_caching_with_filepath(self, m_sfw, m_pickle):
-        expected_cache_path = '/some/file/path'
-        expected_pickle_return = 'some pickle value'
-
-        m_pickle.dumps.return_value = expected_pickle_return
-
-        dms = DataMemorySet(expected_cache_path)
-        dms.cache()
-
-        m_sfw.assert_called_once_with(expected_cache_path,
-                                      expected_pickle_return)
-
-    @mock.patch('smqtk.representation.data_set.memory_set.pickle')
-    @mock.patch('smqtk.representation.data_set.memory_set.safe_file_write')
-    def test_caching_no_filepath(self, m_sfw, m_pickle):
-        # should not do anything
+    def test_caching_no_map_no_cache(self):
         dms = DataMemorySet()
+        # should do nothing
         dms.cache()
-        ntools.assert_equal(m_sfw.call_count, 0)
-        ntools.assert_equal(m_pickle.call_count, 0)
+        ntools.assert_is_none(dms.cache_element)
+        ntools.assert_equal(dms._element_map, {})
+
+    def test_cacheing_no_map(self):
+        dms = DataMemorySet(DataMemoryElement())
+        dms.cache()
+        # technically caches something, but that something is an empty map.
+        ntools.assert_false(dms.cache_element.is_empty())
+        ntools.assert_equal(pickle.loads(dms.cache_element.get_bytes()),
+                            {})
+
+    def test_cacheing_with_map(self):
+        expected_cache = DataMemoryElement()
+        expected_map = {
+            0: 'a',
+            75: 'b',
+            124769: 'c',
+        }
+
+        dms = DataMemorySet(expected_cache)
+        dms._element_map = expected_map
+        dms.cache()
+
+        ntools.assert_false(expected_cache.is_empty())
+        ntools.assert_equal(pickle.loads(expected_cache.get_bytes()),
+                            expected_map)
+
+    def test_caching_readonly_cache(self):
+        ro_cache = DataMemoryElement(readonly=True)
+        dms = DataMemorySet(ro_cache)
+        ntools.assert_raises(
+            ReadOnlyError,
+            dms.cache
+        )
+
+    def test_get_config_from_config_idempotence(self):
+        default_c = DataMemorySet.get_default_config()
+        ntools.assert_equal(
+            DataMemorySet.from_config(default_c).get_config(),
+            default_c
+        )
+
+        c = DataMemorySet.get_default_config()
+        c['cache_element']['type'] = 'DataMemoryElement'
+        c['cache_element']['DataMemoryElement']['readonly'] = True
+        c['pickle_protocol'] = 1
+        ntools.assert_equal(
+            DataMemorySet.from_config(c).get_config(),
+            c
+        )
 
     def test_count(self):
         expected_map = {

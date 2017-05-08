c626231a65d13a36f74d8575c292cd0e480e8880
Paul Tunison <paul.tunison@kitware.com>
2017-05-08 16:46:22 -0400

    Updated executable compute_hash_codes script for use with KV-stores
    Motivated due to change in LSH structure.

--- python/smqtk/bin/compute_hash_codes.py
+++ python/smqtk/bin/compute_hash_codes.py
@@ -1,17 +1,17 @@
 """
-Compute LSH hash codes based on the provided functor on specific
+Compute LSH hash codes based on the provided functor on all or specific
 descriptors from the configured index given a file-list of UUIDs.
 
 When using an input file-list of UUIDs, we require that the UUIDs of
 indexed descriptors be strings, or equality comparable to the UUIDs' string
 representation.
 
-This script can be used to live update the ``hash2uuid_cache_filepath``
-model file for the ``LSHNearestNeighborIndex`` algorithm as output
-dictionary format is the same as used by that implementation.
+We update a key-value store with the results of descriptor hash computation. We
+assume the keys of the store are the integer hash values and the values of the
+store are ``frozenset`` instances of descriptor UUIDs (hashable-type objects).
+We also assume that no other source is concurrently modifying this key-value
+store due to the need to modify the values of keys.
 """
-
-import cPickle
 import logging
 import os
 
@@ -21,15 +21,17 @@ from smqtk.algorithms import (
 from smqtk.compute_functions import compute_hash_codes
 from smqtk.representation import (
     get_descriptor_index_impls,
+    get_key_value_store_impls,
 )
 from smqtk.utils import (
     bin_utils,
-    file_utils,
     plugin,
 )
 
-
-__author__ = "paul.tunison@kitware.com"
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle
 
 
 def uuids_for_processing(uuids, hash2uuids):
@@ -42,14 +44,14 @@ def uuids_for_processing(uuids, hash2uuids):
 
     :param hash2uuids: Existing mapping of computed hash codes to the UUIDs
         of descriptors that generated the hash.
-    :type hash2uuids: dict[int|long, set[collections.Hashable]]
+    :type hash2uuids: smqtk.representation.KeyValueStore
 
     :return: Iterator over UUIDs to process
     :rtype: __generator[collections.Hashable]
 
     """
     log = logging.getLogger(__name__)
-    already_there = frozenset(v for vs in hash2uuids.itervalues() for v in vs)
+    already_there = frozenset(v for vs in hash2uuids.values() for v in vs)
     skipped = 0
     for uuid in uuids:
         if uuid not in already_there:
@@ -65,11 +67,11 @@ def default_config():
         "utility": {
             "report_interval": 1.0,
             "use_multiprocessing": False,
-            "pickle_protocol": -1,
         },
         "plugins": {
             "descriptor_index": plugin.make_config(get_descriptor_index_impls()),
             "lsh_functor": plugin.make_config(get_lsh_functor_impls()),
+            "hash2uuid_kvstore": plugin.make_config(get_key_value_store_impls()),
         },
     }
 
@@ -85,18 +87,6 @@ def cli_parser():
                            'not provided we compute hash codes for all '
                            'descriptors in the configured descriptor index.')
 
-    g_io.add_argument('--input-hash2uuids',
-                      default=None, metavar="PATH",
-                      help='Optional path to an existing hash2uuids map '
-                           'pickle file to load in for updating. If one is '
-                           'not specified, we create a new map. This file '
-                           'is only modified if it is also the output file.')
-
-    g_io.add_argument('--output-hash2uuids',
-                      metavar="PATH",
-                      help='File path to output updated/new hash2uuids map '
-                           'to, saved in binary pickle format.')
-
     return parser
 
 
@@ -109,17 +99,15 @@ def main():
     # Load configuration contents
     #
     uuid_list_filepath = args.uuids_list
-    hash2uuids_input_filepath = args.input_hash2uuids
-    hash2uuids_output_filepath = args.output_hash2uuids
     report_interval = config['utility']['report_interval']
     use_multiprocessing = config['utility']['use_multiprocessing']
-    pickle_protocol = config['utility']['pickle_protocol']
 
     #
-    # Checking parameters
+    # Checking input parameters
     #
-    if not hash2uuids_output_filepath:
-        raise ValueError("No hash2uuids map output file provided!")
+    if (uuid_list_filepath is not None) and \
+            not os.path.isfile(uuid_list_filepath):
+        raise ValueError("UUIDs list file does not exist!")
 
     #
     # Loading stuff
@@ -136,6 +124,12 @@ def main():
         config['plugins']['lsh_functor'],
         get_lsh_functor_impls()
     )
+    log.info("Loading Key/Value store")
+    #: :type: smqtk.representation.KeyValueStore
+    hash2uuids_kvstore = plugin.from_plugin_config(
+        config['plugins']['hash2uuid_kvstore'],
+        get_key_value_store_impls()
+    )
 
     # Iterate either over what's in the file given, or everything in the
     # configured index.
@@ -150,38 +144,25 @@ def main():
             for k in descriptor_index.iterkeys():
                 yield k
 
-    # load hash2uuids map if it exists, else start with empty dictionary
-    if hash2uuids_input_filepath and os.path.isfile(hash2uuids_input_filepath):
-        log.info("Loading hash2uuids mapping")
-        with open(hash2uuids_input_filepath) as f:
-            hash2uuids = cPickle.load(f)
-    else:
-        log.info("Creating new hash2uuids mapping for output")
-        hash2uuids = {}
-
     #
     # Compute codes
     #
     log.info("Starting hash code computation")
-    compute_hash_codes(
-        uuids_for_processing(iter_uuids(), hash2uuids),
-        descriptor_index,
-        lsh_functor,
-        hash2uuids,
-        report_interval=report_interval,
-        use_mp=use_multiprocessing,
-    )
+    kv_update = {}
+    for uuid, hash_int in compute_hash_codes(uuids_for_processing(iter_uuids(),
+                                                                  hash2uuids_kvstore),
+                                             descriptor_index, lsh_functor,
+                                             report_interval,
+                                             use_multiprocessing, True):
+        # Get original value in KV-store if not in update dict.
+        if hash_int not in kv_update:
+            kv_update[hash_int] = hash2uuids_kvstore.get(hash_int, frozenset())
+        kv_update[hash_int] |= frozenset([uuid])
 
-    #
-    # Output results
-    #
-    tmp_output_filepath = hash2uuids_output_filepath + '.WRITING'
-    log.info("Writing hash-to-uuids map to disk: %s", tmp_output_filepath)
-    file_utils.safe_create_dir(os.path.dirname(hash2uuids_output_filepath))
-    with open(tmp_output_filepath, 'wb') as f:
-        cPickle.dump(hash2uuids, f, pickle_protocol)
-    log.info("Moving on top of input: %s", hash2uuids_output_filepath)
-    os.rename(tmp_output_filepath, hash2uuids_output_filepath)
+    if kv_update:
+        log.info("Updating KV store... (%d keys)" % len(kv_update))
+        hash2uuids_kvstore.add_many(kv_update)
+
     log.info("Done")
 
 

a34b43e66704b750a3c501f765681792443b3491
Paul Tunison <paul.tunison@kitware.com>
2017-06-08 21:50:11 -0400

    Split caffe image into cpu/gpu versions

--- devops/docker/smqtk_caffe/build_image.sh
+++ devops/docker/smqtk_caffe/build_image.sh
@@ -2,9 +2,14 @@
 set -e
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+docker build -t kitware/smqtk/caffe_cpu \
+             -f "${SCRIPT_DIR}/Dockerfile.cpu" \
+             "${SCRIPT_DIR}"
+
 if [ -x "$(which nvidia-docker 2>/dev/null)" ]
 then
     nvidia-docker build -t kitware/smqtk/caffe_nvidia \
+                        -f "${SCRIPT_DIR}/Dockerfile.nvidia_cuDNNv5" \
                         "${SCRIPT_DIR}"
 else
     echo "ERROR: Cannot build image, no nvidia-docker."

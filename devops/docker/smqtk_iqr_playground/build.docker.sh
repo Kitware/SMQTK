bf61c1776e0bc67a834af5da77aaf4dcac8a7fbf
Paul Tunison <paul.tunison@kitware.com>
2017-06-09 00:13:36 -0400

    add custom suffix to docker files in IQR playground
    just for some consistency

--- devops/docker/smqtk_iqr_playground/build.docker.sh
+++ devops/docker/smqtk_iqr_playground/build.docker.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 set -e
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-docker build -t kitware/smqtk/iqr_playground_cpu -f "${SCRIPT_DIR}/Dockerfile" \
+docker build \
+  -t kitware/smqtk/iqr_playground_cpu \
+  -f "${SCRIPT_DIR}/Dockerfile.cpu.df" \
   "${SCRIPT_DIR}"
 
 if [ -x "$(which nvidia-docker 2>/dev/null)" ]
 then
-  nvidia-docker build -t kitware/smqtk/iqr_playground_nvidia \
-    -f "${SCRIPT_DIR}/Dockerfile.nvidia" "${SCRIPT_DIR}"
+  nvidia-docker build \
+    -t kitware/smqtk/iqr_playground_nvidia \
+    -f "${SCRIPT_DIR}/Dockerfile.gpu.df" \
+    "${SCRIPT_DIR}"
 fi

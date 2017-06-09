ccdbd781ec093e0602836d892fbffde829b9b011
Paul Tunison <paul.tunison@kitware.com>
2017-06-08 21:58:41 -0400

    Tweak caffe image build tagging

--- devops/docker/smqtk_caffe/build_image.sh
+++ devops/docker/smqtk_caffe/build_image.sh
@@ -2,13 +2,13 @@
 set -e
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-docker build -t kitware/smqtk/caffe_cpu \
+docker build -t kitware/smqtk/caffe:r5-cpu \
              -f "${SCRIPT_DIR}/Dockerfile.cpu" \
              "${SCRIPT_DIR}"
 
 if [ -x "$(which nvidia-docker 2>/dev/null)" ]
 then
-    nvidia-docker build -t kitware/smqtk/caffe_nvidia \
+    nvidia-docker build -t kitware/smqtk/caffe:r5-gpu-cuda8.0-cudnn5 \
                         -f "${SCRIPT_DIR}/Dockerfile.nvidia_cuDNNv5" \
                         "${SCRIPT_DIR}"
 fi

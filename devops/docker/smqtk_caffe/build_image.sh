9fa12a022362be5ed6ef4ce442f8b06bf6d17321
Paul Tunison <paul.tunison@kitware.com>
2017-07-10 12:04:53 -0400

    Renamed some docker files for some project-specific conventions

--- devops/docker/smqtk_caffe/build_image.sh
+++ devops/docker/smqtk_caffe/build_image.sh
@@ -3,12 +3,12 @@ set -e
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 docker build -t kitware/smqtk/caffe:r5-cpu \
-             -f "${SCRIPT_DIR}/Dockerfile.cpu" \
+             -f "${SCRIPT_DIR}/Dockerfile.cpu.df" \
              "${SCRIPT_DIR}"
 
 if [ -x "$(which nvidia-docker 2>/dev/null)" ]
 then
     nvidia-docker build -t kitware/smqtk/caffe:r5-gpu-cuda8.0-cudnn5 \
-                        -f "${SCRIPT_DIR}/Dockerfile.nvidia_cuDNNv5" \
+                        -f "${SCRIPT_DIR}/Dockerfile.nvidia_cuDNNv5.df" \
                         "${SCRIPT_DIR}"
 fi

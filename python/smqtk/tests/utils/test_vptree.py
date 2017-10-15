ed0570c3a4230d4b733b93a63e1f51257893a2f1
William Hicks <william.p.hicks@gmail.com>
2017-10-15 12:34:49 -0400

    Add tests for VP tree array conversions

--- python/smqtk/tests/utils/test_vptree.py
+++ python/smqtk/tests/utils/test_vptree.py
@@ -6,6 +6,9 @@ import unittest
 
 from six.moves import range
 
+import nose.tools
+import numpy
+
 from smqtk.utils.vptree import *
 
 
@@ -19,6 +22,93 @@ def dist_func_counter(d):
     :type d: dict
     """
     d['count'] = d.get('count', 0) + 1
+
+
+def print_tree(tree, offset=0):
+    ret = "{} {}".format("|"*offset, tree)
+    if tree is not None:
+        ret = "\n".join((
+            ret,
+            print_tree(tree.left, offset=offset+1),
+            print_tree(tree.right, offset=offset+1)
+        ))
+    return ret
+
+
+def assert_vptrees_equal(tree1, tree2):
+    """
+    Assert two vp trees are equivalent
+    :param tree1: Root node of one tree
+    :type tree1: VpNode
+    :param tree2: Root node of other tree
+    :type tree2: VpNode
+    """
+    if tree1 is None or tree2 is None:
+        nose.tools.assert_is(tree1, tree2)
+    else:
+        nose.tools.assert_equal(tree1.p, tree2.p)
+        nose.tools.assert_equal(tree1.p, tree2.p)
+        if tree1.mu is None or tree2.mu is None:
+            nose.tools.assert_is(tree1.mu, tree2.mu)
+        else:
+            numpy.testing.assert_almost_equal(tree1.mu, tree2.mu)
+        assert_vptrees_equal(tree1.left, tree2.left)
+        assert_vptrees_equal(tree1.right, tree2.right)
+
+
+def assert_vpstrees_equal(tree1, tree2):
+    """
+    Assert two vps trees are equivalent
+    :param tree1: Root node of one tree
+    :type tree1: VpsNode
+    :param tree2: Root node of other tree
+    :type tree2: VpsNode
+    """
+    if tree1 is None or tree2 is None:
+        nose.tools.assert_is(tree1, tree2)
+    else:
+        nose.tools.assert_equal(tree1.p, tree2.p)
+        nose.tools.assert_equal(tree1.p, tree2.p)
+        nose.tools.assert_sequence_equal(tree1.bounds, tree2.bounds)
+        if tree1.mu is None or tree2.mu is None:
+            nose.tools.assert_is(tree1.mu, tree2.mu)
+        else:
+            numpy.testing.assert_almost_equal(tree1.mu, tree2.mu)
+        assert_vpstrees_equal(tree1.left, tree2.left)
+        assert_vpstrees_equal(tree1.right, tree2.right)
+
+
+def assert_vpsbtrees_equal(tree1, tree2):
+    """
+    Assert two vpsb trees are equivalent
+    :param tree1: Root node of one tree
+    :type tree1: VpsbNode
+    :param tree2: Root node of other tree
+    :type tree2: VpsbNode
+    """
+    if tree1 is None or tree2 is None:
+        nose.tools.assert_is(tree1, tree2)
+    else:
+        nose.tools.assert_equal(tree1.p, tree2.p)
+        nose.tools.assert_equal(tree1.p, tree2.p)
+        nose.tools.assert_sequence_equal(tree1.bounds, tree2.bounds)
+        if tree1.mu is None or tree2.mu is None:
+            nose.tools.assert_is(tree1.mu, tree2.mu)
+        else:
+            for mu1, mu2 in zip(tree1.mu, tree2.mu):
+                if mu1 is None or mu2 is None:
+                    nose.tools.assert_is(mu1, mu2)
+                else:
+                    numpy.testing.assert_almost_equal(mu1, mu2)
+        if tree1.max_children is None or tree2.max_children is None:
+            nose.tools.assert_is(tree1.max_children, tree2.max_children)
+        else:
+            numpy.testing.assert_equal(tree1.max_children, tree2.max_children)
+        if tree1.children is None or tree2.children is None:
+            nose.tools.assert_is(tree1.children, tree2.children)
+        else:
+            for child1, child2 in zip(tree1.children, tree2.children):
+                assert_vpsbtrees_equal(child1, child2)
 
 
 class TestVpBase (object):
@@ -60,6 +150,20 @@ class TestVpTree (unittest.TestCase, TestVpBase):
         # Calls to distance function should be <= O(n*log(n))
         max_expected_calls = len(S) * math.log(len(S), 2)
         self.assertLessEqual(c['count'], max_expected_calls)
+
+    def test_array_conversion(self):
+        S = self._make_sequential_set()
+        c = {}
+        dist_func = self._make_dist_func(max(S), c)
+        vpt = vp_make_tree(S, dist_func, r_seed=self.RAND_SEED)
+        vp_arrays = vpt.to_arrays()
+        vpt2 = VpNode.from_arrays(
+            vp_arrays['value_array'],
+            vp_arrays['mu_array'],
+            vp_arrays['nd_array'],
+        )
+        assert_vptrees_equal(vpt, vpt2)
+
 
     def test_knn_recursive(self):
         S = self._make_sequential_set()
@@ -139,6 +243,20 @@ class TestVpsTree (unittest.TestCase, TestVpBase):
         # Calls to distance function should be <= O(n*log(n))
         max_expected_calls = len(S) * math.log(len(S), 2)
         self.assertLessEqual(c['count'], max_expected_calls)
+
+    def test_array_conversion(self):
+        S = self._make_sequential_set()
+        c = {}
+        dist_func = self._make_dist_func(max(S), c)
+        vpt = vps_make_tree(S, dist_func, r_seed=self.RAND_SEED)
+        vp_arrays = vpt.to_arrays()
+        vpt2 = VpsNode.from_arrays(
+            vp_arrays['value_array'],
+            vp_arrays['mu_array'],
+            vp_arrays['nd_array'],
+            vp_arrays['bounds_array'],
+        )
+        assert_vpstrees_equal(vpt, vpt2)
 
     def test_knn_recursive(self):
         S = self._make_sequential_set()
@@ -233,6 +351,20 @@ class TestVpsbTree (unittest.TestCase, TestVpBase):
         max_expected_calls = len(S) * math.log(len(S), 9) + (len(S) * 0.1)
         self.assertLessEqual(c['count'], max_expected_calls)
         c['count'] = 0
+
+    def test_array_conversion(self):
+        S = self._make_sequential_set()
+        c = {}
+        dist_func = self._make_dist_func(max(S), c)
+        vpt = vpsb_make_tree(S, dist_func, r_seed=self.RAND_SEED)
+        vp_arrays = vpt.to_arrays()
+        vpt2 = VpsbNode.from_arrays(
+            vp_arrays['value_array'],
+            vp_arrays['mu_array'],
+            vp_arrays['nd_array'],
+            vp_arrays['bounds_array'],
+        )
+        assert_vpsbtrees_equal(vpt, vpt2)
 
     def test_make_deduplicate(self):
         # Make set with duplicate elements (two copies of each integer).

a5c5a738d64043671d9e2bea5f31302657f9ad23
Paul Tunison <paul.tunison@kitware.com>
2017-10-26 16:29:43 -0400

    Remove use of nose in LinearHashIndex tests

--- python/smqtk/tests/algorithms/nn_index/hash_index/test_linear.py
+++ python/smqtk/tests/algorithms/nn_index/hash_index/test_linear.py
@@ -1,6 +1,5 @@
 import unittest
 
-import nose.tools
 import numpy
 
 from smqtk.algorithms.nn_index.hash_index.linear import LinearHashIndex
@@ -11,38 +10,38 @@ class TestLinearHashIndex (unittest.TestCase):
 
     def test_is_usable(self):
         # Should always be true since this impl does no have special deps.
-        nose.tools.assert_true(LinearHashIndex.is_usable())
+        self.assertTrue(LinearHashIndex.is_usable())
 
     def test_default_config(self):
         c = LinearHashIndex.get_default_config()
-        nose.tools.assert_equal(len(c), 1)
-        nose.tools.assert_is_none(c['cache_element']['type'])
+        self.assertEqual(len(c), 1)
+        self.assertIsNone(c['cache_element']['type'])
 
     def test_from_config_no_cache(self):
         # Default config is valid and specifies no cache.
         c = LinearHashIndex.get_default_config()
         i = LinearHashIndex.from_config(c)
-        nose.tools.assert_is_none(i.cache_element)
-        nose.tools.assert_equal(i.index, set())
+        self.assertIsNone(i.cache_element)
+        self.assertEqual(i.index, set())
 
     def test_from_config_with_cache(self):
         c = LinearHashIndex.get_default_config()
         c['cache_element']['type'] = "DataMemoryElement"
         i = LinearHashIndex.from_config(c)
-        nose.tools.assert_is_instance(i.cache_element, DataMemoryElement)
-        nose.tools.assert_equal(i.index, set())
+        self.assertIsInstance(i.cache_element, DataMemoryElement)
+        self.assertEqual(i.index, set())
 
     def test_get_config(self):
         i = LinearHashIndex()
 
         # Without cache element
         expected_c = LinearHashIndex.get_default_config()
-        nose.tools.assert_equal(i.get_config(), expected_c)
+        self.assertEqual(i.get_config(), expected_c)
 
         # With cache element
         i.cache_element = DataMemoryElement()
         expected_c['cache_element']['type'] = 'DataMemoryElement'
-        nose.tools.assert_equal(i.get_config(), expected_c)
+        self.assertEqual(i.get_config(), expected_c)
 
     def test_build_index_no_cache(self):
         i = LinearHashIndex()
@@ -51,8 +50,8 @@ class TestLinearHashIndex (unittest.TestCase):
                        [1, 0, 0],
                        [0, 1, 1],
                        [0, 0, 1]])
-        nose.tools.assert_equal(i.index, {1, 2, 3, 4})
-        nose.tools.assert_is_none(i.cache_element)
+        self.assertEqual(i.index, {1, 2, 3, 4})
+        self.assertIsNone(i.cache_element)
 
     def test_build_index_with_cache(self):
         cache_element = DataMemoryElement()
@@ -62,14 +61,13 @@ class TestLinearHashIndex (unittest.TestCase):
                        [1, 0, 0],
                        [0, 1, 1],
                        [0, 0, 1]])
-        nose.tools.assert_equal(i.index, {1, 2, 3, 4})
-        nose.tools.assert_false(cache_element.is_empty())
+        self.assertEqual(i.index, {1, 2, 3, 4})
+        self.assertFalse(cache_element.is_empty())
 
     def test_build_index_no_input(self):
         i = LinearHashIndex()
-        nose.tools.assert_raises_regexp(
+        self.assertRaises(
             ValueError,
-            "No hashes given to index",
             i.build_index, []
         )
 
@@ -81,16 +79,16 @@ class TestLinearHashIndex (unittest.TestCase):
                        [0, 1, 1],
                        [0, 0, 1]])
         near_codes, near_dists = i.nn([0, 0, 0], 4)
-        nose.tools.assert_equal(set(map(tuple, near_codes[:2])),
-                                {(0, 1, 0), (0, 0, 1)})
-        nose.tools.assert_equal(set(map(tuple, near_codes[2:])),
-                                {(1, 1, 0), (0, 1, 1)})
+        self.assertEqual(set(map(tuple, near_codes[:2])),
+                         {(0, 1, 0), (0, 0, 1)})
+        self.assertEqual(set(map(tuple, near_codes[2:])),
+                         {(1, 1, 0), (0, 1, 1)})
         numpy.testing.assert_array_almost_equal(near_dists,
                                                 (1/3., 1/3., 2/3., 2/3.))
 
     def test_save_cache(self):
         cache_element = DataMemoryElement()
-        nose.tools.assert_true(cache_element.is_empty())
+        self.assertTrue(cache_element.is_empty())
 
         i = LinearHashIndex(cache_element)
         # noinspection PyTypeChecker
@@ -98,13 +96,13 @@ class TestLinearHashIndex (unittest.TestCase):
                        [1, 0, 0],
                        [0, 1, 1],
                        [0, 0, 1]])
-        nose.tools.assert_false(cache_element.is_empty())
-        nose.tools.assert_true(len(cache_element.get_bytes()) > 0)
+        self.assertFalse(cache_element.is_empty())
+        self.assertTrue(len(cache_element.get_bytes()) > 0)
 
     def test_save_cache_readonly(self):
         ro_cache = DataMemoryElement(readonly=True)
         i = LinearHashIndex(ro_cache)
-        nose.tools.assert_raises_regexp(
+        self.assertRaisesRegexp(
             ValueError,
             "is read-only",
             i.build_index,
@@ -126,5 +124,5 @@ class TestLinearHashIndex (unittest.TestCase):
         # load called on initialization.
         i2 = LinearHashIndex(cache_element)
 
-        nose.tools.assert_equal(i1.cache_element, i2.cache_element)
-        nose.tools.assert_equal(i1.index, i2.index)
+        self.assertEqual(i1.cache_element, i2.cache_element)
+        self.assertEqual(i1.index, i2.index)

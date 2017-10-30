f00678d3281947ed00c48bb1269c7ad03e14af5c
Paul Tunison <paul.tunison@kitware.com>
2017-10-30 16:27:37 -0400

    Update HashIndex abstract unittests

--- python/smqtk/tests/algorithms/nn_index/hash_index/test_abstract.py
+++ python/smqtk/tests/algorithms/nn_index/hash_index/test_abstract.py
@@ -18,14 +18,14 @@ class DummyHI (HashIndex):
     def count(self):
         return 0
 
-    def build_index(self, hashes):
-        return super(DummyHI, self).build_index(hashes)
+    def _build_index(self, hashes):
+        return
 
-    def update_index(self, hashes):
-        return super(DummyHI, self).update_index(hashes)
+    def _update_index(self, hashes):
+        return
 
-    def nn(self, h, n=1):
-        return super(DummyHI, self).nn(h, n)
+    def _nn(self, h, n=1):
+        return
 
 
 class TestHashIndex (unittest.TestCase):
@@ -38,50 +38,66 @@ class TestHashIndex (unittest.TestCase):
 
     def test_build_index_empty_iter(self):
         idx = DummyHI()
+        idx._build_index = mock.MagicMock()
         self.assertRaisesRegexp(
             ValueError,
-            "No hash vectors.*",
+            str(HashIndex._empty_iterable_exception()),
             idx.build_index, []
         )
+        # Internal method should not have been called
+        idx._build_index.assert_not_called()
 
     def test_build_index_with_values(self):
         idx = DummyHI()
+        idx._build_index = mock.MagicMock()
         # No error should be returned. Returned iterable contents should match
         # input values.
-        ret = idx.build_index([0, 1, 2])
+        idx.build_index([0, 1, 2])
         self.assertSetEqual(
-            set(ret),
+            set(idx._build_index.call_args[0][0]),
             {0, 1, 2}
         )
 
     def test_update_index_empty_iter(self):
         idx = DummyHI()
+        idx._update_index = mock.MagicMock()
         self.assertRaisesRegexp(
             ValueError,
             "No hash vectors.*",
             idx.update_index, []
         )
+        # Internal method should not have been called.
+        idx._update_index.assert_not_called()
 
     def test_update_index_with_values(self):
         idx = DummyHI()
+        idx._update_index = mock.MagicMock()
         # No error should be returned. Returned iterable contents should match
         # input values.
-        ret = idx.update_index([0, 1, 2])
+        idx.update_index([0, 1, 2])
         self.assertSetEqual(
-            set(ret),
+            set(idx._update_index.call_args[0][0]),
             {0, 1, 2}
         )
 
     def test_nn_no_index(self):
         idx = DummyHI()
+        idx._nn = mock.MagicMock()
         self.assertRaises(
             ValueError,
             idx.nn, 'something'
         )
+        # Internal method should not have been called.
+        idx._nn.assert_not_called()
 
     def test_nn_has_count(self):
         idx = DummyHI()
         idx.count = mock.MagicMock()
         idx.count.return_value = 10
+        idx._nn = mock.MagicMock()
         # This call should now pass that count returns something greater than 0.
         idx.nn('dummy')
+        idx._nn.assert_called_with("dummy", 1)
+
+        idx.nn('bar', 10)
+        idx._nn.assert_called_with("bar", 10)

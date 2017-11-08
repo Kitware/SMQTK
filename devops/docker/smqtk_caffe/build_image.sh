1396b6630dd7c8d378d6159c1b57e5b896055154
Paul Tunison <paul.tunison@kitware.com>
2017-11-08 18:04:52 -0500

    Updated caffe docker to release 1.0

--- devops/docker/smqtk_caffe/build_image.sh
+++ devops/docker/smqtk_caffe/build_image.sh
@@ -2,13 +2,13 @@
 set -e
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-docker build -t kitware/smqtk/caffe:r5-cpu \
+docker build -t kitware/smqtk/caffe:1.0-cpu \
              -f "${SCRIPT_DIR}/Dockerfile.cpu.df" \
              "${SCRIPT_DIR}"
 
 if [ -x "$(which nvidia-docker 2>/dev/null)" ]
 then
-    nvidia-docker build -t kitware/smqtk/caffe:r5-gpu-cuda8.0-cudnn5 \
-                        -f "${SCRIPT_DIR}/Dockerfile.nvidia_cuDNNv5.df" \
+    nvidia-docker build -t kitware/smqtk/caffe:1.0-gpu-cuda8.0-cudnn5 \
+                        -f "${SCRIPT_DIR}/Dockerfile.gpu_cuda_cuDNNv5.df" \
                         "${SCRIPT_DIR}"
 fi

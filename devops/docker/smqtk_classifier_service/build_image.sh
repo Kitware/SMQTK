b0f791a3068b8fd53fdb795a6114aaf5aef5f081
Paul Tunison <paul.tunison@kitware.com>
2017-11-10 12:00:56 -0500

    Updated classifier service container

--- devops/docker/smqtk_classifier_service/build_image.sh
+++ devops/docker/smqtk_classifier_service/build_image.sh
@@ -2,13 +2,14 @@
 set -e
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-docker build -t kitware/smqtk/classifier_service:cpu \
+docker build -t kitware/smqtk/classifier_service:0.2-LRD-cpu \
              -f "${SCRIPT_DIR}/Dockerfile.cpu.df" \
              "${SCRIPT_DIR}"
 
 if [ -x "$(which nvidia-docker 2>/dev/null)" ]
 then
-    nvidia-docker build -t kitware/smqtk/classifier_service:gpu-cuda8.0-cudnn5 \
-                        -f "$SCRIPT_DIR/Dockerfile.gpu-cuda8.0-cudnn5.df" \
-                        "${SCRIPT_DIR}"
+    nvidia-docker build \
+        -t kitware/smqtk/classifier_service:0.2-LRD-gpu-cuda8.0-cudnn6 \
+        -f "$SCRIPT_DIR/Dockerfile.gpu-cuda8.0-cudnn6.df" \
+        "${SCRIPT_DIR}"
 fi

fe39f6b216e3eb434973ef6b2e2cb5fd3124b9ae
Neal Siekierski <neal.siekierski@kitware.com>
2018-09-14 15:40:54 -0400

    Revert "Add compatibility check for python 3"
    This reverts commit ca394455b3d86e138bcbd4ada2cd7024c146b935.
    
    This commit conflicts with fixes made in upstream commits
    93ad0e4847658e947d7530ea9bed6ab3e7408256 and
    fdc5aba173e1d9ab75fd42de05769b6492f6f3f7.  We prefer to follow
    upstream.

--- TPL/libsvm-3.1-custom/python/svmutil.py
+++ TPL/libsvm-3.1-custom/python/svmutil.py
@@ -3,8 +3,6 @@ from __future__ import print_function
 
 from svm import *
 
-import sys
-
 def svm_read_problem(data_file_name):
 	"""
 	svm_read_problem(data_file_name) -> [y, x]
@@ -54,10 +52,7 @@ def svm_save_model(model_file_name, model):
 
 	Save a LIBSVM model to the file model_file_name.
 	"""
-	if sys.version_info >= (3, 0):
-		libsvm.svm_save_model(bytes(model_file_name, encoding='utf-8'), model)
-	else:
-		libsvm.svm_save_model(model_file_name, model)
+	libsvm.svm_save_model(model_file_name, model)
 
 def evaluations(ty, pv):
 	"""

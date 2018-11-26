571f84e4c3a415d8991b4bb3ab3181c98f17d8ef
Paul Tunison <paul.tunison@kitware.com>
2018-11-26 14:08:21 -0500

    Add slots and raise helper method to ContentTypeValidator mixin

--- python/smqtk/utils/content_type_validator.py
+++ python/smqtk/utils/content_type_validator.py
@@ -9,6 +9,8 @@ class ContentTypeValidator (object):
     content types relative to the sub-class' function.
     """
 
+    __slots__ = ()
+
     @abc.abstractmethod
     def valid_content_types(self):
         """
@@ -30,3 +32,29 @@ class ContentTypeValidator (object):
         :rtype: bool
         """
         return data_element.content_type() in self.valid_content_types()
+
+    def raise_valid_element(self, data_element, exception=ValueError,
+                            message=None):
+        """
+        Check if the given data element matches a reported valid content type,
+        raising the given exception class (``ValueError`` by default) if not.
+
+        :param smqtk.representation.DataElement data_element:
+             Data element instance to check.
+        :param StandardError exception:
+            Exception to raise if the given element does not report as a valid
+            content type.
+        :param str message:
+            Specific message to provide with a raise exception.
+
+        """
+        if not self.is_valid_element(data_element):
+            if message is None:
+                message = "Data element does not match a content type " \
+                          "reported as valid. Given: \"{}\"." \
+                          .format(data_element.content_type())
+            # noinspection PyCallingNonCallable
+            # - Leave the handling of whether or not an exception is
+            # constructable to the exception class being constructed (user
+            # decision repercussion).
+            raise exception(message)

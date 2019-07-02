6f756c7a02d9843d7ad5a87a0a58a09f79c2c982
Jonathan Beezley <jonathan.beezley@kitware.com>
2019-07-02 08:22:39 -0400

    Fix stub endpoint in iqr service app

--- python/smqtk/web/iqr_service/iqr_server.py
+++ python/smqtk/web/iqr_service/iqr_server.py
@@ -7,12 +7,9 @@ import time
 import traceback
 import uuid
 import io
-<<<<<<< HEAD
 from PIL import Image
 import numpy as np
-=======
-from numpy import random.randint
->>>>>>> 238a087f9906c1c9c67312b7358493bd53093df2
+
 
 import flask
 
@@ -380,24 +377,19 @@ class IqrService (SmqtkWebApp):
         if content_type is None:
             return make_response_json("No content_type provided"), 400
         
-<<<<<<< HEAD
         img_b64 = flask.request.values.get('img_b64', None)
         if img_b64 is None:
             return make_response_json("No image provided"), 400
         try:
-            T_img_string = base64.urlsafe_b64decode(img_b64)
+            T_img_string = base64.b64decode(img_b64)
+            #T_img_array = np.fromstring(T_img_string, dtype=np.uint8) #beware of float64 vs uint8
             img_container = io.BytesIO(T_img_string)
             T_img_PIL = Image.open(img_container)
-=======
-        img = flask.request.values.get('img', None)
-        if img is None:
-            return make_response_json("No image provided"), 400
-        try:
-            T_img = PIL.Image.open(io.BytesIO(img))
->>>>>>> 238a087f9906c1c9c67312b7358493bd53093df2
-        except:
+            
+            #T_img_PIL = Image.open(img)
+        except Exception:
+            flask.current_app.logger.exception('could not open')
             return make_response_json("Image could not be opened."), 400
-
 
         with self.controller:
             if not self.controller.has_session_uuid(sid):
@@ -418,27 +410,18 @@ class IqrService (SmqtkWebApp):
         relevancy_index = iqr_session.IqrSession(pos,
                                       self.rel_index_config,
                                       sub_sid)
-<<<<<<< HEAD
+
         S_img = saliency.generate_saliency_map(T_img_PIL, self.descriptor_generator, relevancy_index, ADJs) #PIL image out
-        S_img_container = io.BytesIO()
-        S_img.save(S_img_container, format='PNG')
-        S_img_container.seek(0)
-=======
-        S_img = saliency.generate_saliency_map(T_img, self.descriptor_generator, relevancy_index, ADJs) #PIL image out
-        S_img_container = io.BytesIO()
-        S_img.save(S_img_container, format='PNG')
->>>>>>> 238a087f9906c1c9c67312b7358493bd53093df2
-        pid = "sa_map"
-        
+
         #https://stackoverflow.com/questions/11017466/flask-to-return-image-stored-in-database/11017839#11017839
         #https://stackoverflow.com/questions/55301037/save-and-send-large-numpy-arrays-with-flask
         #https://stackoverflow.com/questions/33101935/convert-pil-image-to-byte-array
-        return flask.send_file(
-                    S_img_container,
-                    mimetype='image/png',
-                    as_attachment=True,
-                    attachment_filename='%s.png' % pid), 200
-        
+
+        S_img = T_img_PIL
+        S_img_container = io.BytesIO()
+        S_img.save(S_img_container, format='PNG')
+
+        return flask.Response(S_img_container.getvalue(), mimetype='image/png')
 
     def describe_base64_data(self, b64, content_type):
         """
@@ -804,12 +787,8 @@ class IqrService (SmqtkWebApp):
         with self.controller:
             if not self.controller.has_session_uuid(sid):
                 return make_response_json("session id '%s' not found" % sid,
-<<<<<<< HEAD
                                           sid=sid), 404
             iqr_session.IqrSession(self.positive_seed_neighbors,
-=======
-                                          sid=sid), 404iqr_session.IqrSession(self.positive_seed_neighbors,
->>>>>>> 238a087f9906c1c9c67312b7358493bd53093df2
                                       self.rel_index_config,
                                       sid)
             iqrs = self.controller.get_session(sid)

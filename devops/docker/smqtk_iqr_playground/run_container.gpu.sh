2e8d331a4991f7bf342909bb8c3694bad82e41ff
Paul Tunison <paul.tunison@kitware.com>
2020-04-17 16:49:35 -0400

    Fix iqr-playground image build and helper run scripts
    Bring build logic into docker-compose.build.yml file.

--- devops/docker/smqtk_iqr_playground/run_container.gpu.sh
+++ devops/docker/smqtk_iqr_playground/run_container.gpu.sh
@@ -10,21 +10,13 @@
 #
 set -e
 
-CONTAINER_NAME="smqtk_iqr_gpu"
+# Container image to use
+IQR_CONTAINER=kitware/smqtk/iqr_playground
+IQR_CONTAINER_VERSION="latest-gpu-cuda8-cudnn6"
+# Name for run container instance
+CONTAINER_NAME="kitware/smqtk/iqr_playground:gpu-cuda8-cudnn6"
 IQR_GUI_PORT_PUBLISH=5000
 IQR_REST_PORT_PUBLISH=5001
-
-# Replace SOMETHING_HERE by the real version (probably a date)
-IMAGE_VERSION=`docker image ls | grep iqr | awk '{print $2}' | cut -d\- -f1`
-if [ -z "$IMAGE_VERSION" ]; then
-    echo "Unable to determine image version from Docker. Exiting."
-    exit 2
-fi
-
-GPU_SUFFIX=-gpu-cuda8-cudnn6
-
-IQR_CONTAINER=kitware/smqtk/iqr_playground
-IQR_CONTAINER_VERSION="${IMAGE_VERSION}${GPU_SUFFIX}"
 
 if [ -z "$( docker ps -a | grep "${CONTAINER_NAME}" 2>/dev/null )" ]
 then

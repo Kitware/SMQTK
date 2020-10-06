4c11fb2943155a36948e9d0153c293e978986adb
Roni Choudhury <roni.choudhury@kitware.com>
2020-10-06 15:02:50 -0400

    Apply 1 suggestion(s) to 1 file(s)

--- python/smqtk/utils/plugin.py
+++ python/smqtk/utils/plugin.py
@@ -331,7 +331,10 @@ def get_plugins(interface_type, env_var, helper_var,
 
     # Filter and add the direct subclasses of the interface_type.
     if subclasses:
-        class_set.update(cls for cls in interface_type.__subclasses__() if is_valid(cls, log, interface_type.__module__, interface_type))
+        class_set.update(
+            cls for cls in interface_type.__subclasses__()
+            if is_valid(cls, log, interface_type.__module__, interface_type)
+        )
 
     return class_set
 
